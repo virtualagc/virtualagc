4a9f7a5e8535b1e1f3c1eeeef56f3e7e32a9cdd4
info@sandroid.org <info@sandroid.org@65b3f4bb-524e-0410-bec3-a39597da30db>
2009-05-22 21:01:32 +0000

    More fixes for various Comanche 055 source files.

--- Comanche055/P40-P47.s
+++ Comanche055/P40-P47.s
@@ -11,6 +11,8 @@
 #				of the same name, using Comanche055 page 
 #				images.  
 #		2009-05-20 RSB	In S20.1, a DMP DDV was corrected to DMPR DDV.
+#		2009-05-22 RSB	In BESTTRIM, TC PACTOFF corrected to
+#				TS PACTOFF.
 #
 # This source code has been transcribed or otherwise adapted from digitized
 # images of a hardcopy from the MIT Museum.  The digitization was performed
@@ -710,7 +712,7 @@ MASSBACK	CAE	V97VCNTR	# RESTORE PART OF PRE-DECREMENTED MASS
 		EXTEND
 		BZMF	BTRIMR		# NO, NOT TVC YET	
 BESTTRIM	CAE	DELPBAR		# UPDATE TRIMS WITH DELFILTER VALUES
-		TC	PACTOFF
+		TS	PACTOFF
 		CAE	DELYBAR
 		TS	YACTOFF
 BTRIMR		TC	Q
