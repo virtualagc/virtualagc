557085635cc15e2fdc1064d6093cda78b51ec6d6
ohommes <ohommes@65b3f4bb-524e-0410-bec3-a39597da30db>
2009-05-31 15:55:40 +0000

    Reduced the coloring to avoid making things to wild. Now the Keywords, YUL preprocessor, Interpreted code as well as the comments cleanly and nicely are recognizable.

--- trunk/Contributed/SyntaxHighlight/Prettify/lang-aea.js
+++ trunk/Contributed/SyntaxHighlight/Prettify/lang-aea.js
@@ -33,15 +33,13 @@ PR.registerLangHandler(
         [
 	[PR.PR_KEYWORD, /^(?:ADD|ADZ|SUB|SUZ|MPY|MPR|MPZ|DVP|COM|ABS|CLA|CLZ|LDQ|STO|STQ|ALS|LLS|LRS|TRA|TSQ|TMI|TOV|AXT|TIX|DLY|INP|OUT)\b/,null],
 	[PR.PR_TYPE, /^(?:ORG|BSS|BES|SYN|EQU|DEFINE|DEC|OCT|END)\b/,null],
-         [PR.PR_LITERAL,
-          /^[+\-]?(?:0x[0-9a-f]+|E[0-9]+|\d+\/\d+|(?:\.\d+|\d+(?:\.\d*)?)(?:[ed][+\-]?\d+)?)/i],
          // A single quote possibly followed by a word that optionally ends with
          // = ! or ?.
          [PR.PR_LITERAL,
           /^\'(?:-*(?:\w|\\[\x21-\x7e])(?:[\w-]*|\\[\x21-\x7e])[=!?]?)?/],
-         // A word that optionally ends with = ! or ?.
+         // Any word including labels that optionally ends with = ! or ?.
          [PR.PR_PLAIN,
-          /^-*(?:[a-z_]|\\[\x21-\x7e])(?:[\w-]*|\\[\x21-\x7e])[=!?]?/i],
+          /^-*(?:[!-z_]|\\[\x21-\x7e])(?:[\w-]*|\\[\x21-\x7e])[=!?]?/i],
          // A printable non-space non-special character
          [PR.PR_PUNCTUATION, /^[^\w\t\n\r \xA0()\"\\\';]+/]
         ]),
