cd804f5204697673f8ba6fbcd7b25574f29afe23
info@sandroid.org <info@sandroid.org@65b3f4bb-524e-0410-bec3-a39597da30db>
2009-06-02 03:24:11 +0000

    Experimental fix for lst2html.sh on Win32 native builds.  Completely untested on any platform.

--- trunk/VirtualAGC/CopyApolloSource.sh
+++ trunk/VirtualAGC/CopyApolloSource.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # This script is used by the VirtualAGC Makefile to copy AGC or AGS
 # stuff into the installation-build directory.  The usage is:
-#	sh CopyApolloSource.sh dirname sourcename ext
+#	sh CopyApolloSource.sh dirname sourcename ext win
 # where sourcename is something like Luminary131 or FP8 and the 
-# dirname is something .../Resources.  Finally, ext is either -pR
-# for Mac OS X, or -a for everyone else.
+# dirname is something .../Resources.  EXT is either -pR
+# for Mac OS X, or -a for everyone else.  Finally, WIN is either 
+# anything non-blank (for Windows systems) or else blank (for anything
+# other than Windows).
 
 SOURCENAME=$2
 SOURCEDIR=../$SOURCENAME
 DESTDIR=$1/source/$SOURCENAME
 EXT=$3
+WIN=$4
 
 mkdir $DESTDIR
 cp $EXT $SOURCEDIR/*.binsource $SOURCEDIR/*.s $DESTDIR
-sh ./lst2html.sh $SOURCEDIR/$SOURCENAME.lst $DESTDIR/$SOURCENAME.html
+sh ./lst2html.sh $SOURCEDIR/$SOURCENAME.lst $DESTDIR/$SOURCENAME.html $WIN
 cp $EXT $SOURCEDIR/$SOURCENAME.bin $DESTDIR
 cp $EXT $SOURCEDIR/$SOURCENAME.symtab $DESTDIR/$SOURCENAME.bin.symtab
 
