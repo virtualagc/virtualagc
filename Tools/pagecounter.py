72d69c6e72a65bd368ee9d095eb13ceea14b9e07
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2010-02-04 16:53:25 +0000

    Added header, comments.

--- Tools/pagecounter.py
+++ Tools/pagecounter.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 
+# Copyright 2010 Jim lawton <jim dot lawton at gmail dot com>
+# 
+# This file is part of yaAGC. 
+#
+# yaAGC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# yaAGC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with yaAGC; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+# Python script to check the page meta-comments in AGC source modules. 
+# Looks for all .agc files in the current direcotry, and searches them for '## Page'
+# directives. It checks the directives to verify that there are no incorrect page numbers
+# (missing, extra, duplicated, out of sequence). 
+#
+# While the page numbers do not form part of the original AGC source, they are very important
+# in the code conversion process, and in the debugging of errors in the rope binary files.
+
 import sys
 import glob
 
