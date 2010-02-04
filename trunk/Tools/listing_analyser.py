b5897eaa8fa6139a42f47001016151fc600f5432
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2010-02-04 16:53:25 +0000

    Added header, comments.

--- trunk/Tools/listing_analyser.py
+++ trunk/Tools/listing_analyser.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+# Copyright 2010 Jim lawton <jim dot lawton at gmail dot com>
+# 
+# This file is part of yaAGC. 
+#
+# yaAGC is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# yaAGC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with yaAGC; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+# Python script to analyse the yaYUL listing file produced upon building an 
+# AGC system image. This script finds each change of address in the AGC 
+# sources (from the listing) and forms a list of code blocks ordered by 
+# resulting core (ROM) address. This allows differences between core files 
+# to be more easily traced to the relevant sectionsof the AGC sources. 
+ 
 import sys
 import glob
 
