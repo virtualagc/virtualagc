e5237ac6f03a502699c5239686d89e9fdd006efc
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2011-05-03 22:46:26 +0000

    Another fix for label = value statements. This handles unswitched erasable addresses. Still not 100% however.

--- yaYUL/ParseECADR.c
+++ yaYUL/ParseECADR.c
@@ -35,77 +35,103 @@ ParseECADR (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
   Address_t Address;
   int Value, i;
-  IncPc (&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
+  IncPc(&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
   if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
     {
       strcpy (OutRecord->ErrorMessage, "Next code may overflow storage.");
       OutRecord->Warning = 1;
     }
+
   OutRecord->Bank = InRecord->Bank;
   OutRecord->NumWords = 1;
   OutRecord->Words[0] = 0;
+
   if (InRecord->Extend && !InRecord->IndexValid)
     {
       strcpy (OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
       OutRecord->Fatal = 1;
       OutRecord->Extend = 0;
     }
+
   if (InRecord->IndexValid)
     {
       strcpy (OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
       OutRecord->Fatal = 1;
       OutRecord->IndexValid = 0;
     }
+
   i = GetOctOrDec (InRecord->Operand, &Value);
   if (!i && *InRecord->Mod1 == 0)
     {
       IncPc (&InRecord->ProgramCounter, Value, &Address);
+
     DoIt:  
       if (Address.Invalid)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination address not resolved.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
-      if (Address.Constant && !Address.Address && Address.Value < 01400)
+          strcpy (OutRecord->ErrorMessage, "Destination address not resolved.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
+      if (Address.Constant && !Address.Address)
         {
-	  Address.Constant = 0;
-	  Address.Address = 1;
-	  Address.Erasable = 1;
-	  Address.Banked = 0;
-	  Address.SReg = Address.Value;
-	}
+          if (Address.Value < 01400)
+            {
+              // Unswitched erasable.
+              Address.Constant = 0;
+              Address.Address = 1;
+              Address.Erasable = 1;
+              Address.Banked = 0;
+              Address.SReg = Address.Value;
+            }
+          else if (Address.Value < 04000)
+            {
+              // Switched erasable.
+              Address.Constant = 0;
+              Address.Address = 1;
+              Address.Erasable = 1;
+              Address.Banked = 1;
+              Address.EB = Address.Value / 0400;
+              Address.SReg = (Address.Value % 0400) + 01400;
+            }
+        }
+
       if (!Address.Address)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination is not a memory address.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
+          strcpy (OutRecord->ErrorMessage, "Destination is not a memory address.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
       if (!Address.Erasable)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination not in erasable memory.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}	
+          strcpy (OutRecord->ErrorMessage, "Destination not in erasable memory.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
       if (Address.Value < 0 || Address.Value > 03777)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination address out of range.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}	
-      OutRecord->Words[0] = Address.Value;	
+          strcpy (OutRecord->ErrorMessage, "Destination address out of range.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
+      OutRecord->Words[0] = Address.Value;
     }
   else
     {
       // The operand is NOT a number.  Presumably, it's a symbol.
-      i = FetchSymbolPlusOffset (&InRecord->ProgramCounter, 
-                                 InRecord->Operand, 
-				 InRecord->Mod1, &Address);
+      i = FetchSymbolPlusOffset(&InRecord->ProgramCounter, InRecord->Operand, InRecord->Mod1, &Address);
       if (!i)
         goto DoIt;
+
       sprintf(OutRecord->ErrorMessage, "Symbol \"%s\" undefined or offset bad", InRecord->Operand);
       OutRecord->Fatal = 1;
     }
+
   return (0);  
 }
 
