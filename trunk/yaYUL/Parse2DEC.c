9fac7e0028fb6c05bfb5c64ee7ff7a0af2b8eb01
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2011-05-07 20:11:09 +0000

    Only do the scaling trick if the fractional part is non-zero, otherwise just use the value directly. This avoids having to do any guessing at an implicit scale factor.

--- trunk/yaYUL/Parse2DEC.c
+++ trunk/yaYUL/Parse2DEC.c
@@ -17,16 +17,16 @@
   along with yaAGC; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-  Filename:	Parse2DEC.c
-  Purpose:	Assembles the 2DEC pseudo-op.
-  Mode:		04/15/03 RSB.	Began.
-  		07/23/04 RSB.	Added VN.
-		09/04/04 RSB	DEC and 2DEC weren't detecting -0 -- i.e.,
-				-0 was converted to +0.
-		02/28/09 RSB	Several incorrect string-compares 
-				(of the form string=="" rather than
-				string[0]==0) which had for some reason
-				previously been working were fixed.
+  Filename:     Parse2DEC.c
+  Purpose:      Assembles the 2DEC pseudo-op.
+  Mod History:  04/15/03 RSB   Began.
+                07/23/04 RSB   Added VN.
+                09/04/04 RSB   DEC and 2DEC weren't detecting -0 -- i.e.,
+                               -0 was converted to +0.
+                02/28/09 RSB   Several incorrect string-compares 
+                               (of the form string=="" rather than
+                               string[0]==0) which had for some reason
+                               previously been working were fixed.
 */
 
 #include "yaYUL.h"
@@ -36,22 +36,23 @@
 
 //-------------------------------------------------------------------------
 // Converts a string like "E+-n" or "B+-n" to a scale factor.
-
 double
-ScaleFactor (char *s)
+ScaleFactor(char *s)
 {
   int n;
+
   if (*s == 0)
     return (1.0);
+
   if (*s == 'E')
     {
-      n = atoi (s + 1);
-      return (pow (10.0, n));
+      n = atoi(s + 1);
+      return (pow(10.0, n));
     }
   else if (*s == 'B')
     {
-      n = atoi (s + 1);
-      return (pow (2.0, n));
+      n = atoi(s + 1);
+      return (pow(2.0, n));
     }
   else 
     return (1.0);      
@@ -60,74 +61,94 @@ ScaleFactor (char *s)
 //-------------------------------------------------------------------------
 // Returns non-zero on unrecoverable error  We don't do a heckuva lot of 
 // error-checking in this version.
-
 int
-Parse2DECstar (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
+Parse2DECstar(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
-  return (Parse2DEC (InRecord, OutRecord));
+  return (Parse2DEC(InRecord, OutRecord));
 }
 
 int
-Parse2DEC (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
+Parse2DEC(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
   double x;
   int Sign, Value, i;
-  IncPc (&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
+
+  IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
+
   if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
     {
       strcpy (OutRecord->ErrorMessage, "Next code may overflow storage.");
       OutRecord->Warning = 1;
     }
+
   OutRecord->Bank = InRecord->Bank;
   OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
   OutRecord->Words[1] = ILLEGAL_SYMBOL_VALUE;
   OutRecord->NumWords = 2;
+
   if (InRecord->Extend && !InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
       OutRecord->Fatal = 1;
       OutRecord->Extend = 0;
     }
+
   if (InRecord->IndexValid) 
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
       OutRecord->Fatal = 1;
       OutRecord->IndexValid = 0;
     }
+
   if (InRecord->Operand[0] == 0)
     {
-      strcpy (OutRecord->ErrorMessage, "Operand is missing.");
+      strcpy(OutRecord->ErrorMessage, "Operand is missing.");
       OutRecord->Fatal = 1;
       return (0);
     }  
+
   // Under some circumstances, add a default scale factor.
-  if (NULL == strstr (InRecord->Operand, ".") && *InRecord->Mod1 == 0 && 
-     *InRecord->Mod2 == 0)
-    InRecord->Mod1 = "B-28";    
+  if (strstr(InRecord->Operand, ".") == NULL && *InRecord->Mod1 == 0 && *InRecord->Mod2 == 0)
+    InRecord->Mod1 = "B-28";
+
   // Compute the constant as a floating-point number.  
-  x = strtod (InRecord->Operand, NULL) * ScaleFactor (InRecord->Mod1) *
-      ScaleFactor (InRecord->Mod2);
+  x = strtod(InRecord->Operand, NULL) * ScaleFactor(InRecord->Mod1) * ScaleFactor(InRecord->Mod2);
+
   // Convert to 1's complement format.
   Sign = 0;
-  if (InRecord->Operand[0] == '-' /*x < 0*/)
+  if (InRecord->Operand[0] == '-')
     {
+      // x < 0
       Sign = 1;
       x = -x;
     }
-  if (x >= 1.0)
-    return (1);
-  for (Value = 0, i = 0; i < 28; i++)
+
+  if (fmod(x, 1.0) == 0.0)
     {
-      Value = Value << 1;
-      if (x >= 0.5)
-	{
-	  Value++;
-	  x -= 0.5;
-	}
-      x *= 2;
+      // Integer: just convert directly to octal.
+      Value = (int)x;
+    } 
+  else
+    {
+      // Floating point: scale. FP numbers > 1.0 are an error.
+      if (x >= 1.0)
+        return (1);
+
+      for (Value = 0, i = 0; i < 28; i++)
+        {
+          Value = Value << 1;
+          if (x >= 0.5)
+            {
+              Value++;
+              x -= 0.5;
+            }
+          x *= 2;
+        }
+
+      if (x >= 0.5 && Value < 0x0fffffff)
+        Value++;
     }
-  if (x >= 0.5 && Value < 0x0fffffff)
-    Value++;
+
   i = Value & 0x00003fff;
   Value = (Value >> 14) & 0x00003fff;
   if (Sign)
@@ -137,8 +158,10 @@ Parse2DEC (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
       i &= 0x00007fff;
       Value &= 0x00007fff;
     }
+
   OutRecord->Words[0] = Value;
   OutRecord->Words[1] = i;
+
   return (0);
 }
 
@@ -147,129 +170,158 @@ Parse2DEC (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 // error-checking in this version.
  
 int
-ParseDECstar (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
+ParseDECstar(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
-  return (ParseDEC (InRecord, OutRecord));
+  return (ParseDEC(InRecord, OutRecord));
 }
 
 int
-ParseDEC (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
+ParseDEC(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
   double x;
   int Sign, Value, i;
-  IncPc (&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
+  IncPc(&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
   if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
     {
-      strcpy (OutRecord->ErrorMessage, "Next code may overflow storage.");
+      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
       OutRecord->Warning = 1;
     }
+
   OutRecord->Bank = InRecord->Bank;
   OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
   OutRecord->NumWords = 1;
+
   if (InRecord->Extend && !InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
       OutRecord->Fatal = 1;
       OutRecord->Extend = 0;
     }
+
   if (InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
       OutRecord->Fatal = 1;
       OutRecord->IndexValid = 0;
     }
+
   if (InRecord->Operand[0] == 0)
     {
-      strcpy (OutRecord->ErrorMessage, "Operand is missing.");
+      strcpy(OutRecord->ErrorMessage, "Operand is missing.");
       OutRecord->Fatal = 1;
       return (0);
-    }  
+    }
+
   // Under some circumstances, add a default scale factor.
-  if (NULL == strstr (InRecord->Operand, ".") && *InRecord->Mod1 == 0 && 
-      *InRecord->Mod2 == 0)
-    InRecord->Mod1 = "B-14";    
+  if (strstr(InRecord->Operand, ".") == NULL && *InRecord->Mod1 == 0 && *InRecord->Mod2 == 0)
+    InRecord->Mod1 = "B-14";
+
   // Compute the constant as a floating-point number.  
-  x = strtod (InRecord->Operand, NULL) * ScaleFactor (InRecord->Mod1) * 
-      ScaleFactor (InRecord->Mod2);
+  x = strtod(InRecord->Operand, NULL) * ScaleFactor(InRecord->Mod1) * ScaleFactor(InRecord->Mod2);
+
   // Convert to 1's complement format.
   Sign = 0;
-  if (InRecord->Operand[0] == '-' /*x < 0*/)
+  if (InRecord->Operand[0] == '-')
     {
+      // x < 0
       Sign = 1;
       x = -x;
     }
-  if (x >= 1.0)
-    return (1);
-  for (Value = 0, i = 0; i < 14; i++)
+
+  if (fmod(x, 1.0) == 0.0)
     {
-      Value = Value << 1;
-      if (x >= 0.5)
-	{
-	  Value++;
-	  x -= 0.5;
-	}
-      x *= 2;
+      // Integer: just convert directly to octal.
+      Value = (int)x;
+    } 
+  else
+    {
+      // Floating point: scale. FP numbers > 1.0 are an error.
+      if (x >= 1.0)
+        return (1);
+
+      for (Value = 0, i = 0; i < 14; i++)
+        {
+          Value = Value << 1;
+          if (x >= 0.5)
+            {
+              Value++;
+              x -= 0.5;
+            }
+          x *= 2;
+        }
+
+      if (x >= 0.5 && Value < 0x03fff)
+        Value++;
     }
-  if (x >= 0.5 && Value < 0x03fff)
-    Value++;
+
   if (Sign)
     Value = 0x7FFF & ~Value;
+
   OutRecord->Words[0] = Value;
+
   return (0);
 }
 
 //----------------------------------------------------------------------------
 // VN is a slightly-changed knockoff of DEC, designed to pack verb/noun
 // specs into a single word of memory.
-
 int
-ParseVN (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
+ParseVN(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
   char c;
   unsigned Value;
-  IncPc (&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
+  IncPc(&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
   if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
     {
-      strcpy (OutRecord->ErrorMessage, "Next code may overflow storage.");
+      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
       OutRecord->Warning = 1;
     }
+
   OutRecord->Bank = InRecord->Bank;
   OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
   OutRecord->NumWords = 1;
+
   if (InRecord->Extend && !InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
       OutRecord->Fatal = 1;
       OutRecord->Extend = 0;
     }
+
   if (InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
       OutRecord->Fatal = 1;
       OutRecord->IndexValid = 0;
     }
+
   if (InRecord->Operand[0] == 0)
     {
-      strcpy (OutRecord->ErrorMessage, "Operand is missing.");
+      strcpy(OutRecord->ErrorMessage, "Operand is missing.");
       OutRecord->Fatal = 1;
       return (0);
-    }  
+    }
+
   // The idea here is that the operand is a decimal number, of which the
   // lower two digits are simply placed in the output, whereas the upper two
   // digits are multiplied by 128 before been added to the output.
   // (Isn't the sscanf clever?  The final %c checks for garbage following
   // the decimal number.)
-  if (1 != sscanf (InRecord->Operand,"%u%c", &Value, &c))    
+ 
+  if (sscanf(InRecord->Operand,"%u%c", &Value, &c) != 1)
     {
-      strcpy (OutRecord->ErrorMessage, "Operand is not a decimal number.");
+      strcpy(OutRecord->ErrorMessage, "Operand is not a decimal number.");
       OutRecord->Fatal = 1;
       return (0);
     } 
+
   Value = (Value % 100) | ((Value / 100) << 7); 
-    
   OutRecord->Words[0] = Value;
+
   return (0);
 }
-
-
 
