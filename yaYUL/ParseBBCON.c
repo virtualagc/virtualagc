d4ed292c56e59643963b000b7464fb0d1345c46c
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2011-05-16 20:04:33 +0000

    Remove hard tabs. Remove ORIGINAL_SURMISE defines, use only original-surmise code for s-bank calculation.

--- yaYUL/ParseBBCON.c
+++ yaYUL/ParseBBCON.c
@@ -17,9 +17,9 @@
   along with yaAGC; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-  Filename:	ParseBBCON.c
-  Purpose:	Assembles the BBCON pseudo-ops.
-  Mode:		07/21/04 RSB.	Adapted from ParseECADR.c.
+  Filename:     ParseBBCON.c
+  Purpose:      Assembles the BBCON pseudo-ops.
+  Mode:         07/21/04 RSB.   Adapted from ParseECADR.c.
 */
 
 #include "yaYUL.h"
@@ -31,90 +31,101 @@
 // Returns non-zero on unrecoverable error.
 
 int
-ParseBBCON (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
+ParseBBCON(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
   Address_t Address, EBank;
   int Value, i;
-  IncPc (&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+
+  IncPc(&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
   if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
     {
-      strcpy (OutRecord->ErrorMessage, "Next code may overflow storage.");
+      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
       OutRecord->Warning = 1;
     }
+
   OutRecord->Bank = InRecord->Bank;
   OutRecord->NumWords = 1;
   OutRecord->Words[0] = 0;
+
   if (InRecord->Extend && !InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
       OutRecord->Fatal = 1;
       OutRecord->Extend = 0;
     }
+
   if (InRecord->IndexValid)
     {
-      strcpy (OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
+      strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
       OutRecord->Fatal = 1;
       OutRecord->IndexValid = 0;
     }
-  #ifdef ORIGINAL_SBANK_SURMISE
-  if (!strcmp (InRecord->Operator, "BBCON*"))
+
+  if (!strcmp(InRecord->Operator, "BBCON*"))
     {
       OutRecord->Words[0] = 066100;
       OutRecord->Bank.CurrentSBank.Super = 1;
       return (0);
     }
-  #endif
-  i = GetOctOrDec (InRecord->Operand, &Value);
+
+  i = GetOctOrDec(InRecord->Operand, &Value);
   if (!i && *InRecord->Mod1 == 0)
     {
-      IncPc (&InRecord->ProgramCounter, Value, &Address);
+      IncPc(&InRecord->ProgramCounter, Value, &Address);
     DoIt:  
       if (Address.Invalid)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination address not resolved.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
+          strcpy(OutRecord->ErrorMessage, "Destination address not resolved.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
       if (!Address.Address)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination is not a memory address.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
+          strcpy(OutRecord->ErrorMessage, "Destination is not a memory address.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
       if (!Address.Fixed)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination is not in fixed memory.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}	
+          strcpy(OutRecord->ErrorMessage, "Destination is not in fixed memory.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
       if (Address.SReg < 02000 || Address.SReg > 07777)
         {
-	  strcpy (OutRecord->ErrorMessage, "Destination address out of range.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}	
+          strcpy(OutRecord->ErrorMessage, "Destination address out of range.");
+          OutRecord->Fatal = 1;
+          return (0);
+        }
+
       //printf ("PC=%o EB=%o,%o FB=%o SReg=%o Super=%o,%o ", 
       //        InRecord->ProgramCounter.SReg, Address.EB, InRecord->Bank.CurrentEBank.EB,
       //      Address.FB, Address.SReg,
       //      Address.Super, InRecord->Bank.CurrentSBank.Super);
+
       if (!Address.Banked)
         Address.Value = Address.SReg / 02000;
       else 
         Address.Value = Address.FB;
+
       EBank = InRecord->Bank.CurrentEBank;
+
       if (EBank.SReg >= 0 && EBank.SReg < 01400)
         EBank.EB = EBank.SReg / 0400;
+
       Address.Value = (Address.Value << 10) | EBank.EB;
+
       // Superbank processing.
-      FixSuperbankBits (InRecord, &Address, &Address.Value);
-      OutRecord->Words[0] = Address.Value;	
+      FixSuperbankBits(InRecord, &Address, &Address.Value);
+      OutRecord->Words[0] = Address.Value;
     }
   else
     {
       // The operand is NOT a number.  Presumably, it's a symbol.
-      i = FetchSymbolPlusOffset (&InRecord->ProgramCounter, 
-                                 InRecord->Operand, 
-				 InRecord->Mod1, &Address);
+      i = FetchSymbolPlusOffset(&InRecord->ProgramCounter, InRecord->Operand, InRecord->Mod1, &Address);
       if (!i)
         goto DoIt;
       sprintf(OutRecord->ErrorMessage, "Symbol \"%s\" undefined or offset bad", InRecord->Operand);
