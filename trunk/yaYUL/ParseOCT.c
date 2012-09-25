9e8e9bb8f5677bebf94492caa1d5231700e30828
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2012-09-25 21:06:30 +0000

    Add support for 2OCT with 2 arguments.

--- trunk/yaYUL/ParseOCT.c
+++ trunk/yaYUL/ParseOCT.c
@@ -1,6 +1,6 @@
 /*
   Copyright 2003-2004 Ronald S. Burkey <info@sandroid.org>
-  
+
   This file is part of yaAGC. 
 
   yaAGC is free software; you can redistribute it and/or modify
@@ -21,7 +21,8 @@
   Purpose:	Assembles the 2DEC pseudo-op.
   Mode:		04/18/03 RSB.	Began.
   		07/23/04 RSB.	Added 2OCT.
-*/
+                2012-09-25 JL   Add support for 2OCT with 2 arguments.
+ */
 
 #include "yaYUL.h"
 #include <stdlib.h>
@@ -33,110 +34,107 @@
 // error-checking in this version.
 int ParseOCT(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
-  char *s;
-  int Value;
+    char *s;
+    int Value;
 
-  IncPc(&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
-  if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
-    {
-      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
-      OutRecord->Warning = 1;
+    IncPc(&InRecord->ProgramCounter, 1, &OutRecord->ProgramCounter);
+    if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow) {
+        strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
+        OutRecord->Warning = 1;
     }
 
-  OutRecord->EBank = InRecord->EBank;
-  OutRecord->SBank = InRecord->SBank;
-  OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
-  OutRecord->NumWords = 1;
+    OutRecord->EBank = InRecord->EBank;
+    OutRecord->SBank = InRecord->SBank;
+    OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
+    OutRecord->NumWords = 1;
 
-  s = InRecord->Operand;
-  if (*s == '+' || *s == '-')
-    s++;
-  for (; *s; s++)
-    if (*s < '0' || *s > '7')
-      break;
+    s = InRecord->Operand;
+    if (*s == '+' || *s == '-')
+        s++;
+    for (; *s; s++)
+        if (*s < '0' || *s > '7')
+            break;
 
-  if (!*s && s != InRecord->Operand)    
-    {  
-      int Minus;
+    if (!*s && s != InRecord->Operand) {
+        int Minus;
 
-      Minus = 0;
-      if (InRecord->Operand[0] == '-')
-        Minus = 1;
+        Minus = 0;
+        if (InRecord->Operand[0] == '-')
+            Minus = 1;
 
-      if (1 == sscanf(&InRecord->Operand[Minus], "%o", &Value))
-	{
-	  if (0 != (Value & ~077777))
-	    {
-	      Value &= 077777;
-	      strcpy(OutRecord->ErrorMessage, "Value out of range.");
-	      OutRecord->Warning = 1;
-	    }
-	  if (Minus)
-	    Value = ~Value;
-	  OutRecord->Words[0] = Value;
-	}
-    }  
-  else
-    {
-      strcpy (OutRecord->ErrorMessage, "Not an octal number.");
-      OutRecord->Fatal = 1;
+        if (sscanf(&InRecord->Operand[Minus], "%o", &Value) == 1) {
+            if ((Value & ~077777) != 0) {
+                Value &= 077777;
+                strcpy(OutRecord->ErrorMessage, "Value out of range.");
+                OutRecord->Warning = 1;
+            }
+            if (Minus)
+                Value = ~Value;
+            OutRecord->Words[0] = Value;
+        }
+    } else {
+        strcpy (OutRecord->ErrorMessage, "Not an octal number.");
+        OutRecord->Fatal = 1;
     }    
-  return (0);
+    return (0);
 }
 
 
 int Parse2OCT(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
-  char *s;
-  int Value;
+    char *s;
+    int Value = 0;
+    int Value1 = 0;
+    int Value2 = 0;
 
-  IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
-  if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
-    {
-      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
-      OutRecord->Warning = 1;
+    IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
+    if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow) {
+        strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
+        OutRecord->Warning = 1;
     }
 
-  OutRecord->EBank = InRecord->EBank;
-  OutRecord->SBank = InRecord->SBank;
-  OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
-  OutRecord->NumWords = 2;
+    OutRecord->EBank = InRecord->EBank;
+    OutRecord->SBank = InRecord->SBank;
+    OutRecord->Words[0] = ILLEGAL_SYMBOL_VALUE;
+    OutRecord->NumWords = 2;
 
-  s = InRecord->Operand;
-  if (*s == '+' || *s == '-')
-    s++;
-  for (; *s; s++)
-    if (*s < '0' || *s > '7')
-      break;
+    s = InRecord->Operand;
 
-  if (!*s && s != InRecord->Operand)    
-    {  
-      int Minus;
+    if (*s == '+' || *s == '-')
+        s++;
+    for (; *s; s++)
+        if (*s < '0' || *s > '7')
+            break;
 
-      Minus = 0;
-      if (InRecord->Operand[0] == '-')
-        Minus = 1;
+    if (!*s && s != InRecord->Operand) {
+        int Minus;
 
-      if (1 == sscanf(&InRecord->Operand[Minus], "%o", &Value))
-	{
-	  if (0 != (Value & ~07777777777))
-	    {
-	      Value &= 07777777777;
-	      strcpy(OutRecord->ErrorMessage, "Value out of range.");
-	      OutRecord->Warning = 1;
-	    }
-	  if (Minus)
-	    Value = ~Value;
-	  OutRecord->Words[0] = 077777 & (Value >> 15);
-	  OutRecord->Words[1] = 077777 & Value;
-	}
-    }  
-  else
-    {
-      strcpy(OutRecord->ErrorMessage, "Not an octal number.");
-      OutRecord->Fatal = 1;
-    }    
-  return (0);
+        Minus = 0;
+        if (InRecord->Operand[0] == '-')
+            Minus = 1;
+
+        if (sscanf(&InRecord->Operand[Minus], "%o", &Value1) == 1) {
+            if (sscanf(InRecord->Mod1, "%o", &Value2) == 1) {
+                Value = ((Value1 & 077777) << 15) | (Value2 & 077777);
+            } else {
+                Value = (Value1 & 07777777777);
+            }
+            if ((Value & ~07777777777) != 0) {
+                Value &= 07777777777;
+                strcpy(OutRecord->ErrorMessage, "Value out of range.");
+                OutRecord->Warning = 1;
+            }
+            if (Minus)
+                Value = ~Value;
+            OutRecord->Words[0] = 077777 & (Value >> 15);
+            OutRecord->Words[1] = 077777 & Value;
+        }
+    } else {
+        strcpy(OutRecord->ErrorMessage, "Not an octal number.");
+        OutRecord->Fatal = 1;
+    }
+
+    return (0);
 }
 
 
