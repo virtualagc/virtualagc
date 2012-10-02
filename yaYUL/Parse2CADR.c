c9b1e885ae610a34b196b3128a04364b00b64a46
jim.lawton <jim.lawton@65b3f4bb-524e-0410-bec3-a39597da30db>
2012-10-02 17:24:55 +0000

    Added note on 2FCADR/2CADR usage.

--- yaYUL/Parse2CADR.c
+++ yaYUL/Parse2CADR.c
@@ -1,6 +1,6 @@
 /*
   Copyright 2004 Ronald S. Burkey <info@sandroid.org>
-  
+
   This file is part of yaAGC. 
 
   yaAGC is free software; you can redistribute it and/or modify
@@ -19,134 +19,124 @@
 
   Filename:     Parse2CADR.c
   Purpose:      Assembles the 2CADR and 2BCADR pseudo-ops.
-  Mode:         07/09/04 RSB.   Adapted from 2FCADR.
-*/
+  History:      07/09/04 RSB.   Adapted from 2FCADR.
+                2012-10-02 JL   Added note.
+ */
 
 #include "yaYUL.h"
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
+/* (JL, 2012-10-02)
+ *
+ * NOTE: 2FCADR is not used by any of the actual flight software sources we currently have.
+ * 2CADR is used instead.
+ */
+
 //-------------------------------------------------------------------------
 // Adjust superbank bits in terms of SBANK= and so on.
 void FixSuperbankBits(ParseInput_t *InRecord, Address_t *Address, int *OutValue)
 {
-  if (Address->Fixed && Address->Banked)
-    {
-      if (Address->FB < 030)
-        {
-          if (InRecord->SBank.current.Super)
-              *OutValue |= 0100;
-          else
-              *OutValue |= 0060;
+    if (Address->Fixed && Address->Banked) {
+        if (Address->FB < 030) {
+            if (InRecord->SBank.current.Super)
+                *OutValue |= 0100;
+            else
+                *OutValue |= 0060;
+        } else if (Address->FB >= 030 && Address->FB <= 033) {
+            if (Address->Super)
+                *OutValue |= 0100;
+            else
+                *OutValue |= 0060;
+        } else if (Address->FB > 033 && Address->FB <= 037) {
+            *OutValue |= 0060;
+        } else {
+            *OutValue |= 0060;
         }
-      else if (Address->FB >= 030 && Address->FB <= 033)
-        {
-          if (Address->Super)
-              *OutValue |= 0100;
-          else
-              *OutValue |= 0060;
-        }
-      else if (Address->FB > 033 && Address->FB <= 037)
-        {
-          *OutValue |= 0060;
-        }
-      else
-          *OutValue |= 0060;
+    } else {
+        *OutValue |= 0060;
     }
-  else
-      *OutValue |= 0060;
 }
 
 //-------------------------------------------------------------------------
 // Returns non-zero on unrecoverable error.
 int Parse2CADR(ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
-  Address_t Address;
-  int i;
+    Address_t Address;
+    int i;
 
-  //printf("--- 2CADR: i/p PC=%o FB=%o S=%d SB=%d\n", 
-  //       InRecord->ProgramCounter.Value, InRecord->ProgramCounter.FB, InRecord->ProgramCounter.Super, InRecord->SBank.current.Super);
+    //printf("--- 2CADR: i/p PC=%o FB=%o S=%d SB=%d\n",
+    //       InRecord->ProgramCounter.Value, InRecord->ProgramCounter.FB, InRecord->ProgramCounter.Super, InRecord->SBank.current.Super);
 
-  IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
-  if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
-    {
-      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
-      OutRecord->Warning = 1;
+    IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
+    if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow) {
+        strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
+        OutRecord->Warning = 1;
     }
 
-  OutRecord->EBank = InRecord->EBank;
-  OutRecord->SBank = InRecord->SBank;
-  OutRecord->NumWords = 2;
-  OutRecord->Words[0] = 0;
-  OutRecord->Words[1] = 0;
+    OutRecord->EBank = InRecord->EBank;
+    OutRecord->SBank = InRecord->SBank;
+    OutRecord->NumWords = 2;
+    OutRecord->Words[0] = 0;
+    OutRecord->Words[1] = 0;
 
-  if (InRecord->Extend && !InRecord->IndexValid)
-    {
-      strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
-      OutRecord->Fatal = 1;
-      OutRecord->Extend = 0;
+    if (InRecord->Extend && !InRecord->IndexValid) {
+        strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
+        OutRecord->Fatal = 1;
+        OutRecord->Extend = 0;
     }
 
-  if (InRecord->IndexValid)
-    {
-      strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
-      OutRecord->Fatal = 1;
-      OutRecord->IndexValid = 0;
+    if (InRecord->IndexValid) {
+        strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
+        OutRecord->Fatal = 1;
+        OutRecord->IndexValid = 0;
     }
-    
-  i = FetchSymbolPlusOffset(&InRecord->ProgramCounter, InRecord->Operand, InRecord->Mod1, &Address);
-  if (i)
-    {
-      sprintf(OutRecord->ErrorMessage, "Symbol \"%s\" undefined or offset bad", InRecord->Operand);
-      OutRecord->Fatal = 1;
-    }
-  else
-    {
-      if (Address.Invalid)
-        {
-          strcpy(OutRecord->ErrorMessage, "Destination address not resolved.");
-          OutRecord->Fatal = 1;
-          return (0);
+
+    i = FetchSymbolPlusOffset(&InRecord->ProgramCounter, InRecord->Operand, InRecord->Mod1, &Address);
+    if (i) {
+        sprintf(OutRecord->ErrorMessage, "Symbol \"%s\" undefined or offset bad", InRecord->Operand);
+        OutRecord->Fatal = 1;
+    } else {
+        if (Address.Invalid) {
+            strcpy(OutRecord->ErrorMessage, "Destination address not resolved.");
+            OutRecord->Fatal = 1;
+            return (0);
         }
 
-      if (!Address.Address)
-        {
-          strcpy(OutRecord->ErrorMessage, "Destination is not a memory address.");
-          OutRecord->Fatal = 1;
-          return (0);
+        if (!Address.Address) {
+            strcpy(OutRecord->ErrorMessage, "Destination is not a memory address.");
+            OutRecord->Fatal = 1;
+            return (0);
         }
 
-      //printf("--- 2CADR: symbol=%o (%o,%o) S=%d\n", Address.Value, Address.FB, Address.SReg, Address.Super);
+        //printf("--- 2CADR: symbol=%o (%o,%o) S=%d\n", Address.Value, Address.FB, Address.SReg, Address.Super);
 
-      OutRecord->Words[0] = Address.SReg;
+        OutRecord->Words[0] = Address.SReg;
 
-      if (InRecord->EBank.current.SReg >= 0 && InRecord->EBank.current.SReg < 01400)
-        OutRecord->Words[1] = InRecord->EBank.current.SReg / 0400;
-      else
-        OutRecord->Words[1] = InRecord->EBank.current.EB;
+        if (InRecord->EBank.current.SReg >= 0 && InRecord->EBank.current.SReg < 01400)
+            OutRecord->Words[1] = InRecord->EBank.current.SReg / 0400;
+        else
+            OutRecord->Words[1] = InRecord->EBank.current.EB;
 
-      if (Address.SReg < 02000)
-        ;
-      else if (Address.SReg <= 07777)
-        {
-          if (Address.SReg < 04000)
-              OutRecord->Words[1] |= (Address.FB << 10);
-          else
-              OutRecord->Words[1] |= ((Address.SReg / 02000) << 10);
+        if (Address.SReg < 02000)
+            ;
+        else if (Address.SReg <= 07777) {
+            if (Address.SReg < 04000)
+                OutRecord->Words[1] |= (Address.FB << 10);
+            else
+                OutRecord->Words[1] |= ((Address.SReg / 02000) << 10);
 
-          // Superbank processing.
-          FixSuperbankBits(InRecord, &Address, &OutRecord->Words[1]);
+            // Superbank processing.
+            FixSuperbankBits(InRecord, &Address, &OutRecord->Words[1]);
+        } else {
+            strcpy(OutRecord->ErrorMessage, "Internal error implementing 2CADR.");
+            OutRecord->Fatal = 1;
+            return (0);
         }
-      else
-        {
-          strcpy(OutRecord->ErrorMessage, "Internal error implementing 2CADR.");
-          OutRecord->Fatal = 1;
-          return (0);
-        }
-      //printf("--- 2CADR: o/p PC=%o FB=%o S=%d\n", OutRecord->ProgramCounter.Value, OutRecord->ProgramCounter.FB, OutRecord->ProgramCounter.Super);
+        //printf("--- 2CADR: o/p PC=%o FB=%o S=%d\n", OutRecord->ProgramCounter.Value, OutRecord->ProgramCounter.FB, OutRecord->ProgramCounter.Super);
     }
 
-  return (0);  
+    return (0);
 }
 
--- yaYUL/Parse2FCADR.c
+++ yaYUL/Parse2FCADR.c
@@ -1,6 +1,6 @@
 /*
   Copyright 2004 Ronald S. Burkey <info@sandroid.org>
-  
+
   This file is part of yaAGC. 
 
   yaAGC is free software; you can redistribute it and/or modify
@@ -17,10 +17,11 @@
   along with yaAGC; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-  Filename:	Parse2FCADR.c
-  Purpose:	Assembles the 2FCADR pseudo-ops.
-  Mode:		07/09/04 RSB.	Created.
-*/
+  Filename:     Parse2FCADR.c
+  Purpose:      Assembles the 2FCADR pseudo-ops.
+  Hsitory:      07/09/04 RSB.   Created.
+                2012-10-02 JL   Added note.
+ */
 
 #include "yaYUL.h"
 #include <stdlib.h>
@@ -30,86 +31,80 @@
 //-------------------------------------------------------------------------
 // Returns non-zero on unrecoverable error.
 
+/* (JL, 2012-10-02)
+ *
+ * NOTE: 2FCADR is not used by any of the actual flight software sources we currently have.
+ * 2CADR is used instead. So, any changes made here will have no effect, except on the
+ * Validation code.
+ */
+
 int
 Parse2FCADR (ParseInput_t *InRecord, ParseOutput_t *OutRecord)
 {
-  Address_t Address;
-  int i;
+    Address_t Address;
+    int i;
 
-  IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
-  if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow)
-    {
-      strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
-      OutRecord->Warning = 1;
-    }
-  OutRecord->EBank = InRecord->EBank;
-  OutRecord->SBank = InRecord->SBank;
-  OutRecord->NumWords = 2;
-  OutRecord->Words[0] = 0;
-
-  if (InRecord->Extend && !InRecord->IndexValid)
-    {
-      strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
-      OutRecord->Fatal = 1;
-      OutRecord->Extend = 0;
+    IncPc(&InRecord->ProgramCounter, 2, &OutRecord->ProgramCounter);
+    if (!OutRecord->ProgramCounter.Invalid && OutRecord->ProgramCounter.Overflow) {
+        strcpy(OutRecord->ErrorMessage, "Next code may overflow storage.");
+        OutRecord->Warning = 1;
     }
 
-  if (InRecord->IndexValid)
-    {
-      strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
-      OutRecord->Fatal = 1;
-      OutRecord->IndexValid = 0;
-    }
-    
-  i = FetchSymbolPlusOffset(&InRecord->ProgramCounter, 
-			    InRecord->Operand, 
-			    InRecord->Mod1, &Address);
-  if (i)
-    {
-      sprintf(OutRecord->ErrorMessage, "Symbol \"%s\" undefined or offset bad", InRecord->Operand);
-      OutRecord->Fatal = 1;
-    }
-  else
-    {
-      if (Address.Invalid)
-	{
-	  strcpy(OutRecord->ErrorMessage, "Destination address not resolved.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
+    OutRecord->EBank = InRecord->EBank;
+    OutRecord->SBank = InRecord->SBank;
+    OutRecord->NumWords = 2;
+    OutRecord->Words[0] = 0;
 
-      if (!Address.Address)
-	{
-	  strcpy(OutRecord->ErrorMessage, "Destination is not a memory address.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
-
-      if (!Address.Fixed /*|| !Address.Banked */)
-	{
-	  strcpy(OutRecord->ErrorMessage, "Destination not in fixed memory.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
-
-      //OutRecord->Words[0] = Address.Value - 010000;	
-      if (Address.SReg >= 02000 && Address.SReg <= 03777)
-        {
-	  OutRecord->Words[0] = ((Address.FB << 10) | (Address.SReg - 02000));
-	  OutRecord->Words[1] = Address.SReg;
-	}
-      else if (Address.SReg >= 04000 && Address.SReg <= 07777)
-        {
-	  OutRecord->Words[0] = Address.SReg;
-	  OutRecord->Words[1] = Address.SReg;
-	}
-      else
-        {
-	  strcpy(OutRecord->ErrorMessage, "Internal error implementing 2FCADR.");
-	  OutRecord->Fatal = 1;
-	  return (0);
-	}
+    if (InRecord->Extend && !InRecord->IndexValid) {
+        strcpy(OutRecord->ErrorMessage, "Illegally preceded by EXTEND.");
+        OutRecord->Fatal = 1;
+        OutRecord->Extend = 0;
     }
-  return (0);  
+
+    if (InRecord->IndexValid) {
+        strcpy(OutRecord->ErrorMessage, "Illegally preceded by INDEX.");
+        OutRecord->Fatal = 1;
+        OutRecord->IndexValid = 0;
+    }
+
+    i = FetchSymbolPlusOffset(&InRecord->ProgramCounter,
+                              InRecord->Operand,
+                              InRecord->Mod1, &Address);
+    if (i) {
+        sprintf(OutRecord->ErrorMessage, "Symbol \"%s\" undefined or offset bad", InRecord->Operand);
+        OutRecord->Fatal = 1;
+    } else {
+        if (Address.Invalid) {
+            strcpy(OutRecord->ErrorMessage, "Destination address not resolved.");
+            OutRecord->Fatal = 1;
+            return (0);
+        }
+
+        if (!Address.Address) {
+            strcpy(OutRecord->ErrorMessage, "Destination is not a memory address.");
+            OutRecord->Fatal = 1;
+            return (0);
+        }
+
+        if (!Address.Fixed /*|| !Address.Banked */) {
+            strcpy(OutRecord->ErrorMessage, "Destination not in fixed memory.");
+            OutRecord->Fatal = 1;
+            return (0);
+        }
+
+        //OutRecord->Words[0] = Address.Value - 010000;
+        if (Address.SReg >= 02000 && Address.SReg <= 03777) {
+            OutRecord->Words[0] = ((Address.FB << 10) | (Address.SReg - 02000));
+            OutRecord->Words[1] = Address.SReg;
+        } else if (Address.SReg >= 04000 && Address.SReg <= 07777) {
+            OutRecord->Words[0] = Address.SReg;
+            OutRecord->Words[1] = Address.SReg;
+        } else {
+            strcpy(OutRecord->ErrorMessage, "Internal error implementing 2FCADR.");
+            OutRecord->Fatal = 1;
+            return (0);
+        }
+    }
+    return (0);
 }
 
