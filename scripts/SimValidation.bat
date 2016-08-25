ae16375072952fc2d8357f2fae27fdc2ec07cbb9
Ron Burkey <rburkey2005@earthlink.net>
2016-08-25 10:59:47 -0500

    Preparing this branch to hold a web-page, by removing all of the existing material.

--- scripts/SimValidation.bat
+++
@@ -1,5 +0,0 @@
-c:
-cd \mingw\bin
-start yaAGC --core=Validation.bin %2
-yaDSKY %1 --cfg=LM.ini
-
--- yaAGC/agc_help.h
+++
@@ -1,7 +0,0 @@
-#ifndef AGC_HELP_H
-#define AGC_HELP_H
-
-extern int GdbmiHelp(char* s);
-extern int legacyHelp(char* s);
-
-#endif
