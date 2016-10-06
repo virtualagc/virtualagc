c03dff9fd0c4a818b9238c8b452e0c358a03dc93
Jim Lawton <jim.lawton@gmail.com>
2016-10-06 20:59:55 +0100

    Transcribe TABLE OF CONTENTS.

--- YUL/TABLE_OF_CONTENTS.s
+++ YUL/TABLE_OF_CONTENTS.s
@@ -22,3 +22,38 @@
 
 ## Note: Page numbers are scan page numbers rather than listing.
 
+R0001   A YUL PROGRAMMER'S DIGEST . . . . . . . . . . . . . . . . . . . . . . .  2
+
+R0002            EXAMPLES OF YUL DIRECTOR CARD TYPES. . . . . . . . . . . . . .  2
+
+R0003            OPTIONAL SUBDIRECTORS. . . . . . . . . . . . . . . . . . . . .  4
+
+R0004            OTHER ASSEMBLY SUBDIRECTORS. . . . . . . . . . . . . . . . . .  5
+
+R0005            SYMBOLIC CARD IMAGE SELECTION PROCESS. . . . . . . . . . . . .  5
+
+R0006            MANUFACTURING SUBDIRECTORS FOR BLK2 AND AGC. . . . . . . . . .  6
+
+R0007            CARD MERGING PROCESS IN ASSEMBLY . . . . . . . . . . . . . . .  7
+R00072                  SEQUENCE BREAKS . . . . . . . . . . . . . . . . . . . .  7
+R00074                  ACCEPTOR CARDS. . . . . . . . . . . . . . . . . . . . .  7
+R00076                  INSERTION CARDS . . . . . . . . . . . . . . . . . . . .  7
+R0008                   PARTIAL RENUMBERING . . . . . . . . . . . . . . . . . .  8
+R0009                   MANIPULATION OF WHOLE LOG SECTIONS. . . . . . . . . . .  8
+R0010                   REJECTED ASSEMBLY (BAD MERGE) . . . . . . . . . . . . .  9
+
+R0011            THE GREAT BIG BAD SECRET . . . . . . . . . . . . . . . . . . . 10
+
+
+R0012   INTERPRETIVE CODING RULES FOR BLK2 (OLD STYLE). . . . . . . . . . . . . 11
+
+
+R0013   INTERPRETIVE CODING RULES FOR AGC  (NEW STYLE). . . . . . . . . . . . . 15
+
+R00132           HOW TO PATCH INTERPRETIVE CODING FOR SIMULATIONS . . . . . . . 19
+
+
+
+
+
+R0014  *               HUGH BLAIR-SMITH / IL7-238E / EXT (1)215 / DECEMBER 1967
