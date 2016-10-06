2329d0735862b04c5d6965a5d85c789d20524ba5
Mike Stewart <mstewart@skybox.com>
2016-10-05 18:52:35 -0700

    yaDSKY2: Made proceed button handling more better (and backwards compatible)

--- yaDSKY2/yaDSKY2.h
+++ yaDSKY2/yaDSKY2.h
@@ -223,7 +223,7 @@ public:
     virtual void on_EntrButton_pressed(wxCommandEvent &event); // wxGlade: <event_handler>
     virtual void on_RsetButton_pressed(wxCommandEvent &event); // wxGlade: <event_handler>
     virtual void on_ThreeButton_pressed(wxCommandEvent &event); // wxGlade: <event_handler>
-    virtual void on_LeftMouse_released(wxMouseEvent &event);
+    virtual void on_ProButton_released(wxCommandEvent &event);
     virtual void HotkeyEvent (wxKeyEvent &event);
 }; // wxGlade: end class
 
