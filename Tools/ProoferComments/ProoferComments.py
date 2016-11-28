57ddf23a2e8d6f71e75b4cf3ec175688a2f6bb6e
Ron Burkey <rburkey2005@earthlink.net>
2016-11-28 10:20:53 -0600

    For reasons I don't grasp, ProoferComments.py was omitted from my last commit.

--- Tools/ProoferComments/ProoferComments.py
+++ Tools/ProoferComments/ProoferComments.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
+# This file was written by Ron Burkey, who declares it to be in the public domain.
+
 # This program is similar to ProoferBox.py, which is used for checking octals
 # from AGC listings, but instead it check the program comments in AGC listings.
 #
 # This program assumes that all "modern" comments are marked with ## or ###, and
 # are completely ignored, except that it assumes page markings of the form "## Page N"
 # are present.
-#
-# AT THE MOMENT THIS CODE IS JUST A CLONE OF ProoferBox.Py, AND ALTHOUGH IT WILL 
-# BE VERY SIMILAR, IT WON'T BE IDENTICAL.
 
 import sys
 import re
+import os.path
 from subprocess import call
 from wand.image import Image, COMPOSITE_OPERATORS
 from wand.drawing import Drawing
@@ -24,53 +24,55 @@ if len(sys.argv) != 5:
 
 backgroundImage = sys.argv[1]
 outImage = sys.argv[2]
-bankNumber = int(sys.argv[3])
-if bankNumber < 0 or bankNumber >= 044:
-	print 'Bank number', bankNumber, 'is out of range.'
-	sys.exit()
-pageInBank = int(sys.argv[4])
-if pageInBank < 0 or pageInBank >= 4:
-	print 'Page', pageInBank, 'is out of range.'
-	sys.exit()
-binsourceFilename = sys.argv[5]
+pageNumber = int(sys.argv[3])
+agcSourceFilename = sys.argv[4]
 
 # Shell out to have tesseract generate the box file, and read it in..
-call([ 'tesseract', backgroundImage, 'octal.burst.exp0', '-psm', '6', 'batch.nochop', 'makebox', 'octals' ])
-file =open ('octal.burst.exp0.box', 'r')
+call([ 'tesseract', backgroundImage, 'eng.burst.exp0', '-psm', '6', 'batch.nochop', 'makebox' ])
+file =open ('eng.burst.exp0.box', 'r')
 boxes = file.readlines()
 file.close()
 
-# Read in the binsource file.
-file = open (binsourceFilename, 'r')
+# Read in the AGC source file.
+file = open (agcSourceFilename, 'r')
 lines = []
-octalPattern = re.compile(r"[0-7]{6}( [0-7]{6}){7}.*")
+currentPage = -1
+blankLinePattern = re.compile(r"\A\s*\Z")
 for line in file:
-	if octalPattern.match(line):
-		lines.append(line)
+	if line.lower().startswith("## page "):
+		fields = line.split()
+		currentPage = int(fields[2])
+		continue
+	if currentPage > pageNumber:
+		break;
+	if currentPage < pageNumber:
+		continue
+	parts = line.partition("#") # Find the start of the comment, if any.
+	if parts[0] == line or parts[2].startswith("#"): # If no comment, or a ##-style comment, ignore the line.
+		continue
+	if re.match(blankLinePattern, parts[2]): # And if the comment itself is blank, ignore the line too.
+		continue;
+	lines.append(parts[2])
 file.close()
-if len(lines) != 044 * 4 * 8 * 4:
-	print "Binsource file", binsourceFilename, "is not 044 banks long."
-	sys.exit()
 
-# Read in the octal-digit files.
-images = []
-images.append(Image(filename='0t.png'))
-images.append(Image(filename='1t.png'))
-images.append(Image(filename='2t.png'))
-images.append(Image(filename='3t.png'))
-images.append(Image(filename='4t.png'))
-images.append(Image(filename='5t.png'))
-images.append(Image(filename='6t.png'))
-images.append(Image(filename='7t.png'))
-imagesColored = []
-imagesColored.append(Image(filename='0m.png'))
-imagesColored.append(Image(filename='1m.png'))
-imagesColored.append(Image(filename='2m.png'))
-imagesColored.append(Image(filename='3m.png'))
-imagesColored.append(Image(filename='4m.png'))
-imagesColored.append(Image(filename='5m.png'))
-imagesColored.append(Image(filename='6m.png'))
-imagesColored.append(Image(filename='7m.png'))
+# At this point, we've populated lines[] with just the non-blank comments from,
+# the selected page, which is precisely what should appear in the box file as well.
+
+# Read in the "font" files.
+imagesMatch = []
+for ascii in range(128):
+	filename = "asciiFont/match" + str(ascii) + ".png"
+	if os.path.isfile(filename):
+		imagesMatch.append(Image(filename=filename))
+	else:
+		imagesMatch.append(Image(filename="asciiFont/127.png"))
+imagesNomatch = []
+for ascii in range(128):
+	filename = "asciiFont/nomatch" + str(ascii) + ".png"
+	if os.path.isfile(filename):
+		imagesNomatch.append(Image(filename=filename))
+	else:
+		imagesNomatch.append(Image(filename="asciiFont/127.png"))
 
 # Read in the input image ... i.e., the B&W octal page.
 img = Image(filename=backgroundImage)
@@ -81,65 +83,71 @@ backgroundHeight = img.height
 img.type = 'truecolor'
 img.alpha_channel = 'activate'
 
-# Determine the range of binsource lines we need to use.  We're guaranteed
-# they're all in the binsource lines[] array.
-if bankNumber < 4:
-	bankNumber = bankNumber ^ 2
-startIndex = bankNumber * 4 * 8 * 4 + pageInBank * 4 * 8
-endIndex = startIndex + 4 * 8
-
-# Loop on lines on the selected page.  We're going to assume that the boxes are
-# in 1-to-1 correspondence with the binsource digit, in the order read from 
-# disk, except that there may be less boxes (on the last page of a bank).
+# Loop on lines on the selected page.  
 draw = Drawing()
 row = 0
-lastRight = 1000000
+lastX = -1 # When the x coordinate of a box suddenly decreases, it marks a new row.
+lastY = 1000000 # Or when the y coordinate increases by more than a pixel or two.
 boxIndex = 0
-for index in range(startIndex, endIndex):
+for row in range(0, len(lines)):
 	if boxIndex >= len(boxes):
 		print 'Out of boxes on page'
 		break
-	# Loop on the octal digits in the row.
-	col = 0
-	characterIndex = 0
-	characters = list(lines[index])
-	for octalDigitIndex in range(0, 8 * 6):
+	# Loop on non-blank characters in the row.
+	characters = list(lines[row])
+	for character in characters:
+		if re.match(blankLinePattern, character):
+			continue
 		if boxIndex >= len(boxes):
 			print 'Out of boxes in row'
 			break
 		# Parse the box entry.
 		boxFields = boxes[boxIndex].split()
-		boxOctal = int(boxFields[0])
+		boxChar = boxFields[0]
 		boxLeft = int(boxFields[1])
 		boxBottom = backgroundHeight -1 - int(boxFields[2])
 		boxRight = int(boxFields[3])
 		boxTop = backgroundHeight - 1 - int(boxFields[4])
 		boxWidth = boxRight + 1 - boxLeft
 		boxHeight = boxBottom + 1 - boxTop
+		if boxLeft < lastX or boxBottom > lastY + 15: # We've run out of boxes on this row.
+			lastX = boxLeft
+			lastY = boxBottom
+			break
+		lastX = boxLeft
+		lastY = boxBottom
 		
-		digitIndex = int(characters[characterIndex])
-		if boxOctal == digitIndex:
-			digit = images[digitIndex].clone()
-			operator = 'darken' #'xor' 
+		asciiCode = ord(character)
+		if boxChar == character:
+			fontChar = imagesMatch[asciiCode].clone()
 		else:
-			digit = imagesColored[digitIndex].clone()
-			operator = 'darken'
-		digit.resize(boxWidth, boxHeight, 'cubic')
-		
-		draw.composite(operator=operator, left=boxLeft, top=boxTop, width=boxWidth, height=boxHeight, image=digit)
-		
-		characterIndex += 1
-		col += 1
-		if (octalDigitIndex % 6) == 4:
-			col += 1
-		if (octalDigitIndex % 6) == 5:
-			col += 7
-			characterIndex += 1
+			fontChar = imagesNomatch[asciiCode].clone()
+		operator = 'darken' 
+		fontChar.resize(boxWidth, boxHeight, 'cubic')
+		draw.composite(operator=operator, left=boxLeft, top=boxTop, width=boxWidth, height=boxHeight, image=fontChar)
 		boxIndex += 1
-	# Next row, please
-	row += 1
-	if (index % 4) == 3:
-		row += 1.2
+	# We're finished with the characters from the comment, but it's conceivable that
+	# there could still be more boxes in this row, because of some kind of mismatch.
+	# We would then need to move along in the boxes[] array until reaching the next line.
+	while 1:
+		if boxIndex >= len(boxes):
+			break
+		# Parse the box entry.
+		boxFields = boxes[boxIndex].split()
+		boxChar = boxFields[0]
+		boxLeft = int(boxFields[1])
+		boxBottom = backgroundHeight -1 - int(boxFields[2])
+		boxRight = int(boxFields[3])
+		boxTop = backgroundHeight - 1 - int(boxFields[4])
+		boxWidth = boxRight + 1 - boxLeft
+		boxHeight = boxBottom + 1 - boxTop
+		if boxLeft < lastX or boxBottom > lastY + 15: # We've run out of boxes on this row.
+			lastX = boxLeft
+			lastY = boxBottom
+			break
+		laxtX = boxLeft
+		lastY = boxBottom
+		boxIndex += 1
 draw(img)
 
 # Create the output image.
