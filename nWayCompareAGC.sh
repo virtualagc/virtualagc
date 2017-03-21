12723f6bc2ba2ba5716c2184999a43687307cc70
Ron Burkey <rburkey2005@earthlink.net>
2017-03-21 11:34:14 -0500

    Modified nWayCompareAGC.sh so that it can optionally use Difdef in place of Diffuse.  This is a first step towards creating an octopus/ProoferComments type system for proofing AGC comment whitespace.  (Or at least, that's the motivation, whether or not it's an actual step.)

--- nWayCompareAGC.sh
+++ nWayCompareAGC.sh
@@ -11,11 +11,27 @@
 # then the normalized files it produces are viewed using the 'Diffuse" utility
 # (http://diffuse.sourceforge.net/).
 
-# Allowing it to operate on code as well (though continuing to ignore whitespace
-# can also be done, with an environment variable.
+# The script can alternately be used with the 'Difdef' utility
+# (https://github.com/Quuxplusone/difdef).  The distinction is that with the
+# default (Diffuse) you get a visual side-by-side comparison, whereas alternatively
+# (Difdef), you get a machine-readable version of the comparison, containing a 
+# list of the unique lines, prepended by a string that indicates *which* of the 
+# input files contains that line.  My idea, specifically, for using Difdef is that
+# it provides a tool for using an octopus/ProoferComments-like process for determining
+# where the whitespace in comments should be, by allowing you to figure out (for any
+# given shared line of source code) which assembly-listing printouts contain it.  One
+# could then use the *best* of those to deduce the whitespace, and then you could 
+# apply that same whitespace for that line to all of the other source file containing it.
+
+# Allowing it to operate on code as well (though continuing to ignore whitespace)
+# can also be done, with an environment variable.  Note that if 'Difdef' is being used,
+# lines consisting *only* of code are still discarded.  (I.e., lines consisting of 
+# comment-only or code+comment are used, but not code-only.)  With 'Diffuse', the 
+# code-only lines are retained.  This, of course, is in line with the usage rationale,
+# since we're only interested in debugging the whitespace in comments, and not in code.
  
 # The usage is:
-#	[CODETOO=yes] nWayCompareAGC.sh Filename.agc Dir1 Dir2 ...
+#	[DIFDEF=yes] [CODETOO=yes] nWayCompareAGC.sh Filename.agc Dir1 Dir2 ...
 # None of the arguments is allowed to contain any spaces.  For example, 
 #	./nWayCompareAGC.sh ALARM_AND_ABORT.agc Luminary069 Luminary099 Luminary131 Luminary210
 
@@ -34,32 +50,71 @@ then
 	WIPECODE=""
 fi
 
+letters=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+letter=0
 for dir in "${DIRS[@]}"
 do
 	if [[ ! -f $dir/$FILE ]]
 	then
-		echo "Input file "$dir/$FILE" does not exist."
-		exit 1
+		if [[ "$DIFDEF" == "yes" ]]
+		then
+			echo "@ $dir/$FILE"
+		else
+			echo "Input file "$dir/$FILE" does not exist."
+			exit 1
+		fi
+	else
+		if [[ "$DIFDEF" == "yes" ]]
+		then
+			echo "${letters[$letter]} $dir/$FILE"
+			letter=$((letter+1))
+		fi
 	fi
 done
+if [[ "$DIFDEF" == "yes" ]]
+then
+	echo "="
+	REPAGE="@@"
+else
+	REPAGE="##"
+fi
 
 START=`pwd`
-mkdir ~/Desktop/nWayCompareAGC
-cd ~/Desktop/nWayCompareAGC
+mkdir ~/Desktop/nWayCompareAGC &>/dev/null
+cd ~/Desktop/nWayCompareAGC &>/dev/null
 
 # Normalize all of the input files.
+if [[ "$DIFDEF" == "yes" ]]
+then
+COMMAND="difdef"
+else
 COMMAND="diffuse -w"
+fi
 for dir in "${DIRS[@]}"
 do
-	COMMAND="$COMMAND $dir.txt"
-	sed -r \
-		-e 's/[#][#].*//' \
-		$WIPECODE \
-		-e 's/[[:space:]]+/ /g' \
-		-e 's/^[#][[:space:]]*$//' \
-		$START/$dir/$FILE | grep --invert-match '^[[:space:]]*$' >$dir.txt
+	if [[ -f $START/$dir/$FILE ]]
+	then
+		COMMAND="$COMMAND $dir.txt"
+		cat $START/$dir/$FILE | \
+		sed -r 's/^\#\#[[:space:]]+Page[[:space:]]+([0-9]+)[[:space:]]*$/'$REPAGE' Page \1/' | \
+		sed -r \
+			-e 's/[#][#].*//' \
+			$WIPECODE \
+			-e 's/[[:space:]]+/ /g' \
+			-e 's/ ([[:punct:]])/\1/g' \
+			-e 's/([[:punct:]]) /\1/g' \
+			-e 's/^[#][[:space:]]*$//' | \
+		grep --invert-match '^[[:space:]]*$' \
+		>$dir.txt
+		if [[ "$DIFDEF" == "yes" ]]
+		then
+			mv $dir.txt temp.txt
+			egrep '(\#|@@)' temp.txt >$dir.txt
+			rm temp.txt
+		fi
+	fi
 done
 
 $COMMAND
 
-cd -
+cd - &>/dev/null
