702d03e9779653eca8875bb58fa30fd0d1aeaa58
Ron Burkey <rburkey2005@earthlink.net>
2017-11-21 14:21:59 -0600

    runPiDSKY2.sh now loops back to mission selection after exiting from the simulation.  All of the extraneous messages have been cleaned up, as well the menu.  Unfortunately, there's no actual 'X' key to press among the physical DSKY's buttons, so there's no way at present to do this loop in the actual hardware.

--- piPeripheral/runPiDSKY2.sh
+++ piPeripheral/runPiDSKY2.sh
@@ -31,65 +31,71 @@ find "$SOURCEDIR" -name "*.ini" -exec cp {} . \;
 cp -a "$SOURCEDIR/yaAGC/yaAGC" .
 cp -a "$SOURCEDIR/yaDSKY2/yaDSKY2" .
 
-# Choose a mission.
-clear
-echo "Available missions:"
-echo "   0 Apollo 5 LM"
-echo "   1 Apollo 8 CM"
-echo "   2 Apollo 9 CM"
-echo "   3 Apollo 10 LM"
-echo "   4 Apollo 11 CM"
-echo "   5 Apollo 11 LM"
-echo "   6 Apollo 12 LM"
-echo "   7 Apollo 13 LM"
-echo "   8 Apollo 15-17 CM"
-echo "   9 Apollo 15-17 LM"
-echo "Default Apollo 11 LM after 10 seconds."
-read -p "Choose one: " -t 10 -n 1
-if [[ "$REPLY" == "0" ]]
-then
-	CORE=Sunburst120
-	CFG=LM0
-elif [[ "$REPLY" == "1" ]]
-then
-	CORE=Colossus237
-	CFG=CM
-elif [[ "$REPLY" == "2" ]]
-then
-	CORE=Colossus249
-	CFG=CM
-elif [[ "$REPLY" == "3" ]]
-then
-	CORE=Luminary069
-	CFG=CM
-elif [[ "$REPLY" == "4" ]]
-then
-	CORE=Comanche055
-	CFG=CM
-elif [[ "$REPLY" == "6" ]]
-then
-	CORE=Luminary116
-	CFG=LM
-elif [[ "$REPLY" == "7" ]]
-then
-	CORE=Luminary131
-	CFG=LM
-elif [[ "$REPLY" == "8" ]]
-then
-	CORE=Artemis072
-	CFG=CM
-elif [[ "$REPLY" == "9" ]]
-then
-	CORE=Luminary210
-	CFG=LM1
-else # $REPLY == 5
-	CORE=Luminary099
-	CFG=LM
-fi
-echo "Running $CORE $CFG"
-
-# Run it!
-rm LM.core CM.core
-./yaAGC --core=$CORE.bin --port=19697 --cfg=$CFG.ini &
-./piDSKY2.py --port=19697 $1
-killall yaAGC
+while true
+do
+	# Choose a mission.
+	clear
+	echo "Available missions:"
+	echo "   0 - Apollo 5 LM"
+	echo "   1 - Apollo 8 CM"
+	echo "   2 - Apollo 9 CM"
+	echo "   3 - Apollo 10 LM"
+	echo "   4 - Apollo 11 CM"
+	echo "   5 - Apollo 11 LM (default)"
+	echo "   6 - Apollo 12 LM"
+	echo "   7 - Apollo 13 LM"
+	echo "   8 - Apollo 15-17 CM"
+	echo "   9 - Apollo 15-17 LM"
+	read -p "Choose a number: " -t 10 -n 1
+	if [[ "$REPLY" == "0" ]]
+	then
+		CORE=Sunburst120
+		CFG=LM0
+	elif [[ "$REPLY" == "1" ]]
+	then
+		CORE=Colossus237
+		CFG=CM
+	elif [[ "$REPLY" == "2" ]]
+	then
+		CORE=Colossus249
+		CFG=CM
+	elif [[ "$REPLY" == "3" ]]
+	then
+		CORE=Luminary069
+		CFG=CM
+	elif [[ "$REPLY" == "4" ]]
+	then
+		CORE=Comanche055
+		CFG=CM
+	elif [[ "$REPLY" == "6" ]]
+	then
+		CORE=Luminary116
+		CFG=LM
+	elif [[ "$REPLY" == "7" ]]
+	then
+		CORE=Luminary131
+		CFG=LM
+	elif [[ "$REPLY" == "8" ]]
+	then
+		CORE=Artemis072
+		CFG=CM
+	elif [[ "$REPLY" == "9" ]]
+	then
+		CORE=Luminary210
+		CFG=LM1
+	else # $REPLY == 5
+		CORE=Luminary099
+		CFG=LM
+	fi
+	echo ""
+	echo "Starting AGC program $CORE ..."
+	
+	# Run it!
+	rm LM.core CM.core &>/dev/null
+	./yaAGC --core=$CORE.bin --port=19697 --cfg=$CFG.ini &>/dev/null &
+	YAGC_PID=$!
+	./piDSKY2.py --port=19697 $1 &>/dev/null
+	echo "Cleaning up ..."
+	kill $YAGC_PID
+	wait $YAGC_PID &>/dev/null
+done
