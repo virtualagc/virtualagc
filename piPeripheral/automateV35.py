44a220cbeaba8fc444e7401e4f220a6c2a4a1946
Ron Burkey <rburkey2005@earthlink.net>
2017-11-28 14:03:22 -0600

    Changed the automateV35 script to do a sequence of several operations, and not just V35E, since I don't seem to be about to detect any further errors with just V35E.

--- piPeripheral/automateV35.py
+++ piPeripheral/automateV35.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Simulates V35E being pressed repeatedly, every 10 seconds, indefinitely, until stopped.
+# Simulates V35E and various other DSKY keypad commands repeatedly until stopped.
+
+print("Focus the 272x480 window within the next 5 seconds!")
 
 # Must do "sudo modprobe uinput" before running this program.  There's also
 # a one-time setup of "sudo pip3 install python-uinput".  This program itself 
@@ -11,18 +13,28 @@ import time
 
 device = uinput.Device([
 	uinput.KEY_V,
+	uinput.KEY_N,
+	uinput.KEY_0,
+	uinput.KEY_1,
+	uinput.KEY_2,
 	uinput.KEY_3,
+	uinput.KEY_4,
 	uinput.KEY_5,
+	uinput.KEY_6,
+	uinput.KEY_7,
+	uinput.KEY_8,
+	uinput.KEY_9,
 	uinput.KEY_ENTER
 ])
 
 interKeyDelay = 0.1
-interCmdDelay = 8
-time.sleep(1)
+time.sleep(5)
 count = 0
 while True:
-	print("V35 count " + str(count))
 	count += 1
+	print("Count " + str(count))
+	
+	print("\tV35E")
 	device.emit_click(uinput.KEY_V)
 	time.sleep(interKeyDelay)
 	device.emit_click(uinput.KEY_3)
@@ -30,5 +42,67 @@ while True:
 	device.emit_click(uinput.KEY_5)
 	time.sleep(interKeyDelay)
 	device.emit_click(uinput.KEY_ENTER)
-	time.sleep(interCmdDelay)
+	time.sleep(7)
 	
+	print("\tV37E00E")
+	device.emit_click(uinput.KEY_V)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_3)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_7)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_ENTER)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_0)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_0)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_ENTER)
+	time.sleep(3)
+	
+	print("\tV91E")
+	device.emit_click(uinput.KEY_V)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_9)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_1)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_ENTER)
+	time.sleep(4)
+	
+	for i in range(0, 5):
+		print("\tV33E")
+		device.emit_click(uinput.KEY_V)
+		time.sleep(interKeyDelay)
+		device.emit_click(uinput.KEY_3)
+		time.sleep(interKeyDelay)
+		device.emit_click(uinput.KEY_3)
+		time.sleep(interKeyDelay)
+		device.emit_click(uinput.KEY_ENTER)
+		time.sleep(4)
+	
+	print("\tV34E")
+	device.emit_click(uinput.KEY_V)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_3)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_4)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_ENTER)
+	time.sleep(3)
+	
+	print("\tV16N65E")
+	device.emit_click(uinput.KEY_V)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_1)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_6)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_N)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_6)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_5)
+	time.sleep(interKeyDelay)
+	device.emit_click(uinput.KEY_ENTER)
+	time.sleep(10)
