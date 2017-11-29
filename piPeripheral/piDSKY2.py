21c88206d5918ebcf5563ff6bd9000637b541193
Ron Burkey <rburkey2005@earthlink.net>
2017-11-29 10:21:21 -0600

    More of the same.

--- piPeripheral/piDSKY2.py
+++ piPeripheral/piDSKY2.py
@@ -302,8 +302,10 @@ if args.pigpio:
 	# using the chip's display-test function.
 	print("SPI test ...")
 	for i in range(0, 10):
+		print("On " + str(i))
 		writeSpi(15, 1)
 		time.sleep(1)
+		print("Off " + str(i))
 		writeSpi(15, 0)
 		time.sleep(1)
 	print("SPI test completed.")
@@ -634,21 +636,21 @@ def vnFlashingStop():
 # Which of the two sets of parameters ends up being used is dependendent on the 
 # --pigpio command-line parameter. 
 lampStatuses = {
-	"UPLINK ACTY" : { "isLit" : False, "cliParameter" : "3", "spiParameters" : [ { register:1, mask:0xf0 } ] },
-	"TEMP" : { "isLit" : False, "cliParameter" : "2", "spiParameters" : [ { register:1, mask:0x0f } ] },
-	"NO ATT" : { "isLit" : False, "cliParameter" : "5", "spiParameters" : [ { register:2, mask:0xf0 } ] },
-	"GIMBAL LOCK" : { "isLit" : False, "cliParameter" : "4", "spiParameters" : [ { register:2, mask:0x0f } ] },
-	"DSKY STANDBY" : { "isLit" : False, "cliParameter" : "7", "spiParameters" : [ { register:3, mask:0xf0 } ] },
-	"PROG" : { "isLit" : False, "cliParameter" : "6", "spiParameters" : [ { register:3, mask:0x0f } ] },
-	"OPR ERR" : { "isLit" : False, "cliParameter" : "9", "spiParameters" : [ { register:4, mask:0xf0 } ] },
-	"RESTART" : { "isLit" : False, "cliParameter" : "8", "spiParameters" : [ { register:4, mask:0x0f } ] },
-	"KEY REL" : { "isLit" : False, "cliParameter" : "B", "spiParameters" : [ { register:5, mask:0xf0 } ] },
-	"TRACKER" : { "isLit" : False, "cliParameter" : "A", "spiParameters" : [ { register:5, mask:0x0f } ] },
-	"ALT" : { "isLit" : False, "cliParameter" : "C", "spiParameters" : [ { register:6, mask:0xf0 } ] },
-	"VEL" : { "isLit" : False, "cliParameter" : "E", "spiParameters" : [ { register:6, mask:0x0f } ] },
-	"PRIO DSP" : { "isLit" : False, "cliParameter" : "D", "spiParameters" : [ { register:7, mask:0xf0 } ] },
-	"NO DAP" : { "isLit" : False, "cliParameter" : "F", "spiParameters" : [ { register:7, mask:0x0f } ] },
-	"VNCSERVERUI" : { "isLit" : False, "cliParameter" : "G", "spiParameters" : [ { register:8, mask:0xff } ] }
+	"UPLINK ACTY" : { "isLit" : False, "cliParameter" : "3", "spiParameters" : [ { "register":1, "mask":0xf0 } ] },
+	"TEMP" : { "isLit" : False, "cliParameter" : "2", "spiParameters" : [ { "register":1, "mask":0x0f } ] },
+	"NO ATT" : { "isLit" : False, "cliParameter" : "5", "spiParameters" : [ { "register":2, "mask":0xf0 } ] },
+	"GIMBAL LOCK" : { "isLit" : False, "cliParameter" : "4", "spiParameters" : [ { "register":2, "mask":0x0f } ] },
+	"DSKY STANDBY" : { "isLit" : False, "cliParameter" : "7", "spiParameters" : [ { "register":3, "mask":0xf0 } ] },
+	"PROG" : { "isLit" : False, "cliParameter" : "6", "spiParameters" : [ { "register":3, "mask":0x0f } ] },
+	"OPR ERR" : { "isLit" : False, "cliParameter" : "9", "spiParameters" : [ { "register":4, "mask":0xf0 } ] },
+	"RESTART" : { "isLit" : False, "cliParameter" : "8", "spiParameters" : [ { "register":4, "mask":0x0f } ] },
+	"KEY REL" : { "isLit" : False, "cliParameter" : "B", "spiParameters" : [ { "register":5, "mask":0xf0 } ] },
+	"TRACKER" : { "isLit" : False, "cliParameter" : "A", "spiParameters" : [ { "register":5, "mask":0x0f } ] },
+	"ALT" : { "isLit" : False, "cliParameter" : "C", "spiParameters" : [ { "register":6, "mask":0xf0 } ] },
+	"VEL" : { "isLit" : False, "cliParameter" : "E", "spiParameters" : [ { "register":6, "mask":0x0f } ] },
+	"PRIO DSP" : { "isLit" : False, "cliParameter" : "D", "spiParameters" : [ { "register":7, "mask":0xf0 } ] },
+	"NO DAP" : { "isLit" : False, "cliParameter" : "F", "spiParameters" : [ { "register":7, "mask":0x0f } ] },
+	"VNCSERVERUI" : { "isLit" : False, "cliParameter" : "G", "spiParameters" : [ { "register":8, "mask":0xff } ] }
 }
 #lampCliStringDefault = "FIJKLMNOPQRSTUVWXd"
 lampCliStringDefault = ""
