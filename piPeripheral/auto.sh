a97c909e5267744984d4cd3f366c45f8e92a25fa
Ron Burkey <rburkey2005@earthlink.net>
2017-12-27 14:10:17 -0600

    Tweaked auto.sh program comments a bit more.

--- piPeripheral/auto.sh
+++ piPeripheral/auto.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This script is intended to be used to autostart runPiDSKY2.sh upon
 # Raspberry Pi power-up.  It is assumed that the desktop is automatically
-# logged in.  Then, assuming this script is in /home/pi/virtualagc/piPeripheral/
+# logged in.  Then, assuming this script is in /home/pi/virtualagc/piPeripheral/,
 # the line
+#
 #	@/home/pi/virtualagc/piPeripheral/auto.sh
 #
 # should be added to the end of the file
