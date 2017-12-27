815b1b83d36eee7aa9dd2644c14f7fdb63221ba4
Ron Burkey <rburkey2005@earthlink.net>
2017-12-27 14:08:01 -0600

    Added some comments to auto.sh to explain how to use it to autostart runPiDSKY2.sh on a Raspbian system.

--- piPeripheral/auto.sh
+++ piPeripheral/auto.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# This script is intended to be used to autostart runPiDSKY2.sh upon
+# Raspberry Pi power-up.  It is assumed that the desktop is automatically
+# logged in.  Then, assuming this script is in /home/pi/virtualagc/piPeripheral/
+# the line
+#	@/home/pi/virtualagc/piPeripheral/auto.sh
+#
+# should be added to the end of the file
+#
+#	/home/pi/.config/lxsession/LXDE-pi/autostart
+
 cd /home/pi/virtualagc/piPeripheral
 xterm -fullscreen  -e ./runPiDSKY2.sh --custom-bare --pigpio=7 --playback &
