c76581b5d97624ddb15ab6f7245cd4eb712f4fa9
Ron Burkey <rburkey2005@earthlink.net>
2018-08-14 11:47:49 -0500

    Forgot that I needed the connector-generating script to allow for a starting pin number other than 1.

--- Scripts/MakeConnector.py
+++ Scripts/MakeConnector.py
@@ -6,10 +6,12 @@
 #	Allows (with 0 or 180 degree rotations and DeMorgan) wire entry to the top or bottom.
 #	Pin name at center and always upright (not printed along the y-axis).
 
-# Tweak the stuff below.  All numbers in mils unless specified.
+# Tweak the stuff below.
 partName = "AConnector"
 numPins = 10 # Number of pins
+startingPinNumber = 1
 
+#  All numbers in mils.
 refdPattern = "J"
 xRadius = 225
 yRadius = 175
@@ -30,14 +32,14 @@ print("F3 \"\" " + str(-lineWidth) + " " + str(yRadius) + " " + str(lineWidth) +
 print("DRAW")
 print("A " + str(-lineWidth) + " 0 " + str(yRadius) + " 901 -901 0 0 0 N " + str(-lineWidth) + " " + str(yRadius) + " " + str(-lineWidth) + " " + str(-yRadius))
 print("A " + str(lineWidth) + " 0 " + str(yRadius) + " -899 899 0 0 0 N " + str(lineWidth) + " " + str(-yRadius) + " " + str(lineWidth) + " " + str(yRadius))
-for pin in range(1, numPins+1):
-	print("T 0 0 0 " + str(textSize) + " 0 " + str(pin) + " 0 " + str(pin) + " Normal 0 C C")
+for pin in range(numPins):
+	print("T 0 0 0 " + str(textSize) + " 0 " + str(pin+1) + " 0 " + str(pin+startingPinNumber) + " Normal 0 C C")
 print("P 2 0 0 0 " + str(-lineWidth) + " " + str(-yRadius) + " " + str(lineWidth) + " " + str(-yRadius) + " N")
 print("P 2 0 0 0 " + str(-lineWidth) + " " + str(yRadius) + " " + str(lineWidth) + " " + str(yRadius) + " N")
-for pin in range(1, numPins+1):
-	print("X " + str(pin) + " " + str(pin) + " " + str(xRadius) + " 0 0 L " + str(textSize) + " " + str(textSize) + " " + str(pin) + " 1 P")
-for pin in range(1, numPins+1):
-	print("X " + str(pin) + " " + str(pin) + " 0 " + str(yRadius) + " 0 D " + str(textSize) + " " + str(textSize) + " " + str(pin) + " 2 P")
+for pin in range(numPins):
+	print("X " + str(pin+startingPinNumber) + " " + str(pin+startingPinNumber) + " " + str(xRadius) + " 0 0 L " + str(textSize) + " " + str(textSize) + " " + str(pin+1) + " 1 P")
+for pin in range(numPins):
+	print("X " + str(pin+startingPinNumber) + " " + str(pin+startingPinNumber) + " 0 " + str(yRadius) + " 0 D " + str(textSize) + " " + str(textSize) + " " + str(pin+1) + " 2 P")
 print("ENDDRAW")
 print("ENDDEF")
 print("#")
