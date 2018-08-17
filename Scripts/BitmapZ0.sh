be4d71e7056565f744f88ce28b2ff6dfd93de71a
Ron Burkey <rburkey2005@earthlink.net>
2018-08-17 15:14:15 -0500

    Added a script that could move a background image in a .sch file to Z-level 0, pending (or in lieu) of a change in KiCad itself to allow that.

---
+++ Scripts/BitmapZ0.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# In a KiCad schematics file containing a single image (plus other non-image
+# stuff) moves the image to the very beginning, so that it is "behind" the
+# other objects in display order.
+
+if [[ ! -f "$1" ]]
+then
+	echo "File \"$1\" does not exist."
+	exit 1
+fi
+if [[ 1 != "`grep -c '^\$Bitmap$' 2005259A-p2of2.sch`" ]]
+then 
+	echo "No image or more than 1 image in the file ... won't change."
+	exit 1
+fi	
+
+# Fetch different chunks of the file into temporary files.
+#
+#	temp1.txt	Contains up to (but not including) "$EndDescr"
+#	temp2.txt	Contains "$EndDescr" up to (but not including) "$Bitmap"
+#	temp3.txt	Contains "$Bitmap" up to (but not including) "$EndBitmap"
+#	temp4.txt 	Contains "$EndBitmap" through the end of the file. 
+awk '/^EESchema|^\$EndDescr$|^\$Bitmap$|^\$EndBitmap$/{n++}{print >"temp" n ".txt"}' "$1"
+
+# Backup 
+cp -a "$1" "$1".bak
+
+# Reassemble.
+cat temp1.txt >temp.txt
+echo '$EndDescr' >>temp.txt
+cat temp3.txt >>temp.txt
+echo '$EndBitmap' >>temp.txt
+tail -n +2 temp2.txt >> temp.txt
+tail -n +2 temp4.txt >>temp.txt
+
+mv temp.txt "$1"
