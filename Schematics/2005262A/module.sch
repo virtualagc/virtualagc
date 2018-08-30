0028f7e5651398a62b65865a817c65a6e9cd920f
Ron Burkey <rburkey2005@earthlink.net>
2018-08-30 18:34:22 -0500

    The CAD files for 2005262A (module A4 for AGC 2003200 and 2003993) have been added.  Also, there is now a python script (autoplaceKiCad.py) to assist in putting the symbols onto any of the so-called "logic flow diagrams", and I used it for page 2 of 2005262A; whether it actually *is* an assistance or not is unclear just yet, but it doesn't seem to hurt the process any.

---
+++ Schematics/2005262A/module.sch
@@ -0,0 +1,115 @@
+EESchema Schematic File Version 4
+LIBS:module-cache
+EELAYER 26 0
+EELAYER END
+$Descr E 44000 34000
+encoding utf-8
+Sheet 1 3
+Title ""
+Date ""
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+$Sheet
+S 17375 14775 3275 4875
+U 5B86CFFC
+F0 "Sheet 1" 140
+F1 "2005262A-p1of2.sch" 140
+F2 "FAP" U R 20650 16500 140
+F3 "0VDCA" U R 20650 17925 140
+F4 "+4SW" U R 20650 17225 140
+$EndSheet
+$Sheet
+S 24450 14750 3300 4925
+U 5B86D026
+F0 "Sheet 2" 140
+F1 "2005262A-p2of2.sch" 140
+F2 "FAP" U L 24450 16500 140
+F3 "+4SW" U L 24450 17225 140
+F4 "0VDCA" U L 24450 17925 140
+$EndSheet
+Wire Wire Line
+	20650 17925 22475 17925
+Wire Wire Line
+	24450 17225 22475 17225
+Wire Wire Line
+	20650 16500 22475 16500
+$Comp
+L AGC_DSKY:PWR_FLAG #FLG01
+U 1 1 5B872C9E
+P 22475 16500
+F 0 "#FLG01" H 22475 17025 50  0001 C CNN
+F 1 "PWR_FLAG" H 22485 16960 50  0001 C CNN
+F 2 "" H 22475 16500 50  0001 C CNN
+F 3 "~" H 22475 16500 50  0001 C CNN
+	1    22475 16500
+	1    0    0    -1  
+$EndComp
+Connection ~ 22475 16500
+Wire Wire Line
+	22475 16500 24450 16500
+$Comp
+L AGC_DSKY:PWR_FLAG #FLG02
+U 1 1 5B872CC2
+P 22475 17225
+F 0 "#FLG02" H 22475 17750 50  0001 C CNN
+F 1 "PWR_FLAG" H 22485 17685 50  0001 C CNN
+F 2 "" H 22475 17225 50  0001 C CNN
+F 3 "~" H 22475 17225 50  0001 C CNN
+	1    22475 17225
+	1    0    0    -1  
+$EndComp
+Connection ~ 22475 17225
+Wire Wire Line
+	22475 17225 20650 17225
+$Comp
+L AGC_DSKY:PWR_FLAG #FLG03
+U 1 1 5B872CE6
+P 22475 17925
+F 0 "#FLG03" H 22475 18450 50  0001 C CNN
+F 1 "PWR_FLAG" H 22485 18385 50  0001 C CNN
+F 2 "" H 22475 17925 50  0001 C CNN
+F 3 "~" H 22475 17925 50  0001 C CNN
+	1    22475 17925
+	1    0    0    -1  
+$EndComp
+Connection ~ 22475 17925
+Wire Wire Line
+	22475 17925 24450 17925
+Wire Notes Line width 6 style solid
+	36461 982  36461 1300
+Wire Notes Line width 6 style solid
+	36464 1300 43500 1300
+Wire Notes Line width 6 style solid
+	42480 984  42480 1300
+Wire Notes Line width 6 style solid
+	41831 984  41831 1300
+Wire Notes Line width 6 style solid
+	41331 984  41331 1300
+Wire Notes Line width 6 style solid
+	40831 984  40831 1298
+Wire Notes Line width 6 style solid
+	36839 984  36839 1300
+Wire Notes Line width 6 style solid
+	37350 983  37350 1300
+Text Notes 36560 1274 0    160  ~ 32
+A
+Text Notes 37382 1274 0    160  ~ 32
+REVISED PER TDRR 30754
+Text Notes 38892 31752 0    250  ~ 50
+LOGIC FLOW DIAGRAM
+Text Notes 40852 33152 0    250  ~ 50
+2005262
+Text Notes 38252 33168 0    250  ~ 50
+80230
+Text Notes 39644 32072 0    200  ~ 40
+MODULE NO.A4
+Text Notes 38476 32384 0    180  ~ 36
+STAGE BRANCH DECODING MODULE
+Text Notes 38748 33464 0    140  ~ 28
+NONE
+$EndSCHEMATC
---
+++ Scripts/autoplaceKiCad.py
@@ -0,0 +1,285 @@
+#! /usr/bin/python
+# The purpose of this python script is to take a text file that has some
+# descriptions of NOR gates, expander gates, connector pads, nodes,
+# and perhaps other objects commonly in AGC "logic flow diagrams", and
+# produces a rough placement of those objects that can be cut-and-pasted
+# into an eeschema .sch file.
+
+import sys
+import time
+
+# Current origin and index around the origin.
+spacing = 0.75 # inches
+originX = 1.25 # inches
+originY = 1.25
+nextX = 0
+nextY = 0
+def nextXY():
+	global nextX, nextY
+	if nextX == 0:
+		nextX = nextY + 1
+		nextY = 0
+		return
+	if nextY < nextX:
+		nextY += 1
+		return
+	nextX -= 1
+	return
+
+# For entities with no obvious sequencing.
+index = 0
+
+# A dictionary for storing the objects in the schematic.
+objects = {}
+
+nors = { 
+	"N": { "library": "D3NOR-+4VDC-0VDCA", "refdPrefix": "U1" },
+	"N2": { "library": "D3NOR-+4SW-0VDCA", "refdPrefix": "U2" }, 
+	"X": { "library": "D3NOR-FAP-0VDCA-expander", "refdPrefix": "U1" }, 
+	"X2": { "library": "D3NOR-NC-0VDCA-expander", "refdPrefix": "U2" }
+}
+aPins = ["A", "B", "C", "_"]
+bPins = ["D", "E", "F", "_"]
+
+# Read the input file.
+for line in sys.stdin:
+	fields = line.strip().split(" ")
+	numFields = len(fields)
+	type = fields[0]
+	if numFields < 1 or type == "#":
+		continue
+	if type == "N=" and numFields == 3:
+		nors["N"][library] = fields[1]
+		nors["N"][refdPrefix] = fields[2]
+		continue
+	if type == "N2=" and numFields == 3:
+		nors["N2"][library] = fields[1]
+		nors["N2"][refdPrefix] = fields[2]
+		continue
+	if type == "X=" and numFields == 3:
+		nors["X"][library] = fields[1]
+		nors["X"][refdPrefix] = fields[2]
+		continue
+	if type == "X2=" and numFields == 3:
+		nors["X2"][library] = fields[1]
+		nors["X2"][refdPrefix] = fields[2]
+		continue
+	if type == "L" and numFields == 3:
+		originX = float(fields[1])
+		originY = float(fields[2])
+		nextX = 0
+		nextY = 0
+		continue
+
+	posX = int(1000 * (originX + spacing * nextX))
+	posY = int(1000 * (originY + spacing * nextY))
+	
+	if type in nors and numFields == 6:
+		gate = fields[1]
+		location = fields[2]
+		top = fields[3]
+		middle = fields[4]
+		bottom = fields[5]
+		id = type + location
+		if not (id in objects):
+			objects[id] = { "type": type, "library": nors[type]["library"], "refd": nors[type]["refdPrefix"]+location, "location": location }
+		if top in aPins and middle in aPins and bottom in aPins:
+			whichGate = "a"
+		elif top in bPins and middle in bPins and bottom in bPins:
+			whichGate = "b"
+		else:
+			print >>sys.stderr, "Illegal spec: " + line.strip('\n')
+			sys.exit()
+		objects[id][whichGate] = { "gate": gate, "top": top, "middle": middle, "bottom": bottom, "x": posX, "y": posY } 
+		nextXY()
+		continue
+	
+	if type == "J" and (numFields == 2 or numFields == 3):
+		pinName = fields[1]
+		pinNum = int(pinName)
+		if numFields >= 3:
+			text = fields[2]
+		else:
+			text = ""
+		id = type + pinName
+		if pinNum >= 101 and pinNum <= 171:
+			refd = "J1"
+			symbol = "ConnectorA1-100"
+			unit = pinNum % 100
+		elif pinNum >= 201 and pinNum <= 271:
+			refd = "J2"
+			symbol = "ConnectorA1-200"
+			unit = pinNum % 100
+		elif pinNum >= 301 and pinNum <= 371:
+			refd = "J3"
+			symbol = "ConnectorA1-300"
+			unit = pinNum % 100
+		elif pinNum >= 401 and pinNum <= 471:
+			refd = "J4"
+			symbol = "ConnectorA1-400"
+			unit = pinNum % 100
+		else:
+			print >>sys.stderr, "Illegal pin number: " + line.strip('\n')
+			sys.exit()
+		objects[id] = { "type": type, "refd": refd, "symbol": symbol, "unit": unit, "text": text, "x": posX, "y": posY }
+		nextXY()
+		continue
+	
+	if type == "O" and numFields == 2:
+		id = type + str(index)
+		index += 1
+		text = fields[1]
+		objects[id] = { "type": type, "text": text, "x": posX, "y": posY }
+		nextXY()
+		continue
+	
+	if type == "A" and numFields == 2:
+		id = type + str(index)
+		index += 1
+		count = "(" + fields[1] + ")"
+		objects[id] = { "type": type, "count": count, "x": posX, "y": posY }
+		nextXY()
+		continue		
+
+# Write the output file.
+timestamp = "%X" % time.time()
+for id in objects:
+	object = objects[id]
+	type = object["type"]
+	
+	if type in nors:
+		library = object["library"]
+		refd = object["refd"]
+		location = object["location"]
+		aMirror = False
+		bMirror = False
+		aFootprint = "ABC"
+		bFootprint = "DEF"
+		fontSize = 140
+		xOffset = 0
+		if type in ["X", "X2"]:
+		  fontSize = 130
+		  xOffset = -75
+		if "a" in object:
+			aObject = object["a"]
+			if aObject["top"] < aObject["bottom"]:
+				aFootprint = aObject["top"] + aObject["middle"] + aObject["bottom"]
+			else:
+				aFootprint = aObject["bottom"] + aObject["middle"] + aObject["top"]
+				aMirror = True
+		if "b" in object:
+			bObject = object["b"]
+			if bObject["top"] < bObject["bottom"]:
+				bFootprint = bObject["top"] + bObject["middle"] + bObject["bottom"]
+			else:
+				bFootprint = bObject["bottom"] + bObject["middle"] + bObject["top"]
+				bMirror = True
+		symbol = library + '-' + aFootprint + '-' + bFootprint
+		if "a" in object:
+			unit = 1
+			posX = aObject["x"]
+			posY = aObject["y"]
+			if aMirror:
+				locationOffset = 200
+			else:
+				locationOffset = -200
+			sys.stdout.write("$Comp\n")
+			sys.stdout.write("L " + library + ":" + symbol + " " + refd + "\n")
+			sys.stdout.write("U 1 1 " + timestamp + "\n")
+			sys.stdout.write("P " + str(posX) + " " + str(posY) + "\n")
+			sys.stdout.write("F 0 \"" + refd + "\" H " + str(posX) + " " + str(posY + 325) + " 140 0001 C CNB\n")
+			sys.stdout.write("F 1 \"" + symbol + "\" H " + str(posX) + " " + str(posY+425) + " 140 0001 C CNN\n")
+			sys.stdout.write("F 2 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+			sys.stdout.write("F 3 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+			sys.stdout.write("F 4 \"" + aObject["gate"] + "\" H " + str(posX + xOffset) + " " + str(posY) + " " + str(fontSize) + " 0000 C CNB \"Location\"\n")
+			sys.stdout.write("F 5 \"" + location + "\" H " + str(posX + xOffset) + " " + str(posY + locationOffset) + " " + str(fontSize) + " 0000 C CNB \"Location2\"\n")
+			sys.stdout.write("\t" + str(unit) + " " + str(posX) + " " + str(posY) + "\n")
+			if aMirror:
+				sys.stdout.write("\t1    0    0    1  \n")
+			else:
+				sys.stdout.write("\t1    0    0    -1  \n")
+			sys.stdout.write("$EndComp\n")
+		if "b" in object:
+			unit = 2
+			posX = bObject["x"]
+			posY = bObject["y"]
+			if bMirror:
+				locationOffset = 200
+			else:
+				locationOffset = -200
+			sys.stdout.write("$Comp\n")
+			sys.stdout.write("L " + library + ":" + symbol + " " + refd + "\n")
+			sys.stdout.write("U 2 1 " + timestamp + "\n")
+			sys.stdout.write("P " + str(posX) + " " + str(posY) + "\n")
+			sys.stdout.write("F 0 \"" + refd + "\" H " + str(posX) + " " + str(posY + 325) + " 140 0001 C CNB\n")
+			sys.stdout.write("F 1 \"" + symbol + "\" H " + str(posX) + " " + str(posY+425) + " 140 0001 C CNN\n")
+			sys.stdout.write("F 2 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+			sys.stdout.write("F 3 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+			sys.stdout.write("F 4 \"" + bObject["gate"] + "\" H " + str(posX + xOffset) + " " + str(posY) + " " + str(fontSize) + " 0000 C CNB \"Location\"\n")
+			sys.stdout.write("F 5 \"" + location + "\" H " + str(posX + xOffset) + " " + str(posY + locationOffset) + " " + str(fontSize) + " 0000 C CNB \"Location2\"\n")
+			sys.stdout.write("\t" + str(unit) + " " + str(posX) + " " + str(posY) + "\n")
+			if bMirror:
+				sys.stdout.write("\t1    0    0    1  \n")
+			else:
+				sys.stdout.write("\t1    0    0    -1  \n")
+			sys.stdout.write("$EndComp\n")
+		continue
+	
+	if type == "J":
+		posX = object["x"]
+		posY = object["y"]
+		symbol = object["symbol"]
+		refd = object["refd"]
+		unit = object["unit"]
+		caption = object["text"]
+		sys.stdout.write("$Comp\n")
+		sys.stdout.write("L AGC_DSKY:" + symbol + " " + refd + "\n")
+		sys.stdout.write("U " + str(unit) + " 1 " + timestamp + "\n")
+		sys.stdout.write("P " + str(posX) + " " + str(posY) + "\n")
+		sys.stdout.write("F 0 \"" + refd + "\" H " + str(posX) + " " + str(posY + 325) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 1 \"" + symbol + "\" H " + str(posX) + " " + str(posY+425) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 2 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 3 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 4 \"" + caption + "\" H " + str(posX) + " " + str(posY + 250) + " 140 0000 C BNB \"Caption\"\n")
+		sys.stdout.write("\t" + str(unit) + " " + str(posX) + " " + str(posY) + "\n")
+		sys.stdout.write("\t1    0    0    -1  \n")
+		sys.stdout.write("$EndComp\n")
+		continue
+	
+	if type == "A":
+		unit = 1
+		posX = object["x"]
+		posY = object["y"]
+		number = object["count"]
+		sys.stdout.write("$Comp\n")
+		sys.stdout.write("L AGC_DSKY:ArrowTwiddle X?\n")
+		sys.stdout.write("U 1 1 " + timestamp + "\n")
+		sys.stdout.write("P " + str(posX) + " " + str(posY) + "\n")
+		sys.stdout.write("F 0 \"X?\" H " + str(posX) + " " + str(posY + 325) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 1 \"" + symbol + "\" H " + str(posX) + " " + str(posY+425) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 2 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 3 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 4 \"" + number + "\" H " + str(posX) + " " + str(posY + 400) + " 140 0000 C BNB \"Number\"\n")
+		sys.stdout.write("\t" + str(unit) + " " + str(posX) + " " + str(posY) + "\n")
+		sys.stdout.write("\t1    0    0    -1  \n")
+		sys.stdout.write("$EndComp\n")
+		continue
+	
+	if type == "O":
+		unit = 1
+		posX = object["x"]
+		posY = object["y"]
+		caption = object["text"]
+		sys.stdout.write("$Comp\n")
+		sys.stdout.write("L AGC_DSKY:Node2 X?\n")
+		sys.stdout.write("U 1 1 " + timestamp + "\n")
+		sys.stdout.write("P " + str(posX) + " " + str(posY) + "\n")
+		sys.stdout.write("F 0 \"X?\" H " + str(posX) + " " + str(posY + 325) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 1 \"" + symbol + "\" H " + str(posX) + " " + str(posY+425) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 2 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 3 \"\" H " + str(posX) + " " + str(posY+475) + " 140 0001 C CNN\n")
+		sys.stdout.write("F 4 \"" + caption + "\" H " + str(posX-100) + " " + str(posY) + " 140 0000 R CNB \"Caption\"\n")
+		sys.stdout.write("\t" + str(unit) + " " + str(posX) + " " + str(posY) + "\n")
+		sys.stdout.write("\t1    0    0    -1  \n")
+		sys.stdout.write("$EndComp\n")
+		continue
