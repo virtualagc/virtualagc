1df0a48d8ec10aa3297d8c0fdb3b0ba34426e7ba
Ron Burkey <rburkey2005@earthlink.net>
2018-09-17 14:41:39 -0500

    The new script addBackgroundPNG.py adds a background image to an eeschema file.  It's a little easier to use than doing this through the eeschema UI, since it automatically centers the image and makes sure it draws behinds any other stuff in the schematic already, such as components and wires.  But the image has to be a PNG (unlike the UI, which can accept a JPG), and it doesn't bother to deduce the proper scaling, so you have to already know that (just like using the UI).

---
+++ Scripts/addBackgroundPNG.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# I've become fed up with eeschema's ability to work with background images.
+# I can't do much about most of it, but I can certainly make it easier to 
+# add a background image, position it properly, and scale it, whilst making
+# sure it is at the lowest "Z" level.  I don't pretend any of this is done
+# efficiently, and indeed it's not; it's just done as simply as I could do
+# it without having to learn anything.
+
+import sys
+import os.path
+
+if len(sys.argv) < 4:
+	print >> sys.stderr, "USAGE:"
+	print >> sys.stderr, "\taddBackgroundPNG.py INPUT.sch INPUT.png SCALE"
+	sys.exit(1)
+
+inputSch = sys.argv[1]
+inputPng = sys.argv[2]
+scale = sys.argv[3]
+if not os.path.isfile(inputSch):
+	print >> sys.stderr, "Input schematic " + inputSch + " not found."
+	sys.exit(1)
+if not os.path.isfile(inputPng):
+	print >> sys.stderr, "Input schematic " + inputPng + " not found."
+	sys.exit(1)
+	
+f = open(inputSch, "r")
+schLines = f.readlines()
+f.close()
+numLines = len(schLines)
+for line in range(0, numLines):
+	schLines[line] = schLines[line].rstrip()
+if schLines[0] != "EESchema Schematic File Version 4" or schLines[numLines - 1] != "$EndSCHEMATC":
+	print >> sys.stderr, "Input schematic " + inputSch + " not in expected format. (No eeschema signature.)"
+	sys.exit(1)
+x = 0
+y = 0
+for i in range(1, numLines):
+	fields = schLines[i].split()
+	if fields[0] == "$Descr":
+		x = int(fields[2])
+		y = int(fields[3])
+		break
+if x == 0 or y == 0:
+	print >> sys.stderr, "Input schematic " + inputSch + " not in expected format. (No $Descr.)"
+	sys.exit(1)
+
+EndDescr = 0
+for i in range(2, numLines):
+	if schLines[i] == "$EndDescr":
+		EndDescr = i
+		break
+if EndDescr == 0:
+	print >> sys.stderr, "Input schematic " + inputSch + " not in expected format. (No $EndDescr.)"
+	sys.exit(1)
+
+f = open(inputPng, "rb")
+pngString = f.read()
+f.close()
+pngSize = len(pngString)
+pngBytes = []
+for i in range(0, pngSize):
+	pngBytes.append(ord(pngString[i]))
+del pngString
+pngSignature = [137, 80, 78, 71, 13, 10, 26, 10]
+for i in range(0,8):
+	if pngBytes[i] != pngSignature[i]:
+		print >> sys.stderr, "Input PNG file " + inputPng + " not in expected format."
+		sys.exit(1)
+
+os.rename(inputSch, inputSch + ".bak")
+
+f = open(inputSch, "w")
+for i in range(0, EndDescr + 1):
+	f.write(schLines[i] + "\n")
+f.write("$Bitmap\n")
+f.write("Pos " + str(x//2) + " " + str(y//2) + "\n")
+f.write("Scale " + scale + "\n")
+f.write("Data\n")
+for i in range(0, pngSize):
+	if i % 32 != 0:
+		f.write(" ")
+	f.write("%02X" % pngBytes[i])
+	if i % 32 == 31:
+		f.write("\n")
+if pngSize % 32 != 0:
+	f.write("\n")
+f.write("EndData\n")
+f.write("$EndBitmap\n")
+for i in range(EndDescr + 1, numLines):
+	f.write(schLines[i] + "\n")
+f.close()
