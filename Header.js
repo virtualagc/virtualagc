3c8cf6006feffa170c374cc9b302a63fb44576b5
Ron Burkey <rburkey2005@earthlink.net>
2018-09-23 08:32:20 -0500

    The custom google search isn't working.  This is an attempt to fix it.

--- Header.js
+++ Header.js
@@ -4,7 +4,7 @@
   var gcse = document.createElement('script');
   gcse.type = 'text/javascript';
   gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  gcse.src = 'https://cse.google.com/cse?cx=' + cx;
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(gcse, s);
 })();
