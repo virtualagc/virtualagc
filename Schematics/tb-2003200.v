175883f7d94912b3432fc714f9d7ce44ce5b4541
Ron Burkey <rburkey2005@earthlink.net>
2018-10-11 18:59:16 -0500

    In order to run longer simulations for test purposes, changed the dump file from depth 0 (all symbols) to 1 (just the backplane signals), so as to produce much shorter dump files.  The gtkwave setup file had to be changed also, since in it I had specified some of the signals for the instruction decoder as being the local module symbols rather than the backplane symbols, even though both were available.

--- Schematics/tb-2003200.v
+++ Schematics/tb-2003200.v
@@ -3,9 +3,13 @@
 reg rst = 1;
 initial
   begin
-    $dumpfile("agc.lxt");
-    $dumpvars(0, agc);
+    $dumpfile("agc-short.lxt");
+    // Use 0 to get all of the signals, including those local to individual modules.
+    // Use 1 to get just the backplane signals; this makes the file considerably
+    // shorter.
+    $dumpvars(1, agc);
     # 50 rst = 0;
+    //# 10000000 $finish;
     # 10000000 $finish;
   end
 
@@ -14,6 +18,6 @@ always #2.44140625 CLOCK = !CLOCK;
 
 //reg NHALGA = 1;
 
-//initial $timeformat(-9, 0, " ns", 10);
-//initial $monitor("%t: %d", $time, CLOCK);
+initial $timeformat(-3, 0, " ms", 10);
+always #10000 $display("%t", $time);
 
