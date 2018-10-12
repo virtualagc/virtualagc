1539f2ee1e5abbd7954b1521ed31e61498871669
Ron Burkey <rburkey2005@earthlink.net>
2018-10-12 14:30:59 -0500

    I've readjusted the 2003200 testbench so that it runs for 60 seconds (about 10 seconds longer than Validation should need), and dumps just the signals needed for the instruction decoder, along with A, L, Z, S, FB, and EB (in order to make the dumpfile small).  That should make it as convenient as possible to run an instruction check for Validation vs Mike's sim.

--- Schematics/tb-2003200.v
+++ Schematics/tb-2003200.v
@@ -3,19 +3,46 @@
 reg rst = 1;
 initial
   begin
-    $dumpfile("agc-short.lxt");
-    // Use 0 to get all of the signals, including those local to individual modules.
-    // Use 1 to get just the backplane signals; this makes the file considerably
-    // shorter.
-    $dumpvars(1, agc);
+    $dumpfile("agc.lxt2");
+    // Dump all of the signals, including those local to individual modules.
+    //#dumpvars(0,agc);
+    // Dump just the backplane signals; this makes the file considerably shorter.
+    //$dumpvars(1, agc);
+    // Or, use just the list of variables needed by the instruction decoder.
+    //$dumpvars(1, agc.MINC, agc.DINC, agc.PINC, agc.INKL, agc.T07, agc.T01, agc.TSUDO_, 
+    //	agc.G16, agc.G15, agc.G14, agc.G13, agc.G12, agc.G11, agc.G10, agc.G09, agc.G08, 
+    //	agc.G07, agc.G06, agc.G05, agc.G04, agc.G03, agc.G02, agc.G01, agc.FUTEXT, agc.IC2, 
+    //	agc.STG3, agc.STG2, agc.STG1, agc.WSQG_, agc.GOJAM, agc.RPTFRC, agc.PCDU, agc.MCDU, 
+    //	agc.SHINC, agc.SHANC);
+    // Or, use just the list of variables needed by the instruction decoder, plus a few
+    // more useful things like SAxx and the A, L, and Z registers.
+    $dumpvars(1, agc.MINC, agc.DINC, agc.PINC, agc.INKL, agc.T07, agc.T01, agc.TSUDO_, 
+    	agc.G16, agc.G15, agc.G14, agc.G13, agc.G12, agc.G11, agc.G10, agc.G09, agc.G08, 
+    	agc.G07, agc.G06, agc.G05, agc.G04, agc.G03, agc.G02, agc.G01, agc.FUTEXT, agc.IC2, 
+    	agc.STG3, agc.STG2, agc.STG1, agc.WSQG_, agc.GOJAM, agc.RPTFRC, agc.PCDU, agc.MCDU, 
+    	agc.SHINC, agc.SHANC, agc.S12, agc.S11, agc.S10, agc.S09, 
+    	agc.S08, agc.S07, agc.S06, agc.S05, agc.S04, agc.S03, agc.S02, agc.S01,
+    	agc.Z16_, agc.Z15_, agc.Z14_, agc.Z13_, agc.Z12_, agc.Z11_, agc.Z10_, agc.Z09_, 
+    	agc.Z08_, agc.Z07_, agc.Z06_, agc.Z05_, agc.Z04_, agc.Z03_, agc.Z02_, agc.Z01_,
+    	agc.A16_, agc.A15_, agc.A14_, agc.A13_, agc.A12_, agc.A11_, agc.A10_, agc.A09_, 
+    	agc.A08_, agc.A07_, agc.A06_, agc.A05_, agc.A04_, agc.A03_, agc.A02_, agc.A01_,
+    	agc.FB16, agc.FB14, agc.FB13, agc.FB12, agc.FB11, agc.EB9, agc.EB10, agc.EB11,
+    	agc.L16_, agc.L15_, agc.L14_, agc.L13_, agc.L12_, agc.L11_, agc.L10_, agc.L09_, agc.L08_, 
+    	agc.L07_, agc.L06_, agc.L05_, agc.L04_, agc.L03_, agc.L02_, agc.L01_
+    );
     # 50 rst = 0;
-    //# 10000000 $finish;
-    # 10000000 $finish;
+    //# 10000000000 $finish; // 100 seconds
+    # 600000000 $finish; // 60 seconds ... should be enough time for Validation to run.
+    //# 100000000 $finish; // 10 seconds
+    //# 10000000 $finish; // 1 second
+    //# 100000 $finish; // 10 ms.
+    //# 10000 $finish; // 1 ms.
   end
 
 reg CLOCK = 0;
 always #2.44140625 CLOCK = !CLOCK;
 
+// Turn off parity checking.
 //reg NHALGA = 1;
 
 initial $timeformat(-3, 0, " ms", 10);
