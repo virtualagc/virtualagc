234c3bf8d41be6ee28e58d3b760eb35918657bc2
Ron Burkey <rburkey2005@earthlink.net>
2019-05-06 09:56:38 -0500

    The script that creates the database for the engineering-drawing search engine wasn't normalizing several fields the way I expected (sheet numbers and so on).

--- Scripts/MakeTipueSearch.py
+++ Scripts/MakeTipueSearch.py
@@ -17,9 +17,24 @@ for n in range(0, len(lines)):
 	line = lines[n]
 	fields = line.strip("\n").split("\t")
 	url = fields[0]
-	if fields[5] == "":
-		fields[5] = "1"
-	title = fields[1] + fields[2] + "-" + fields[3] + "-" + fields[4] + "-" + fields[5]
+	number = fields[1]
+	revision = fields[2]
+	type = fields[3]
+	sheet = fields[4]
+	frame = fields[5]
+	while len(type) > 1 and type[0] == '0':
+		type = type[1:]
+	while len(sheet) > 1 and sheet[0] == '0':
+		sheet = sheet[1:]
+	while len(frame) > 1 and frame[0] == '0':
+		frame = frame[1:]
+	if type == "":
+		type = "?"
+	if sheet == "":
+		sheet = "1"
+	if frame == "":
+		frame = "1"
+	title = number + revision + "-" + type + "-" + sheet + "-" + frame
 	count = 0
 	countString = ""
 	if title+countString in titles:
