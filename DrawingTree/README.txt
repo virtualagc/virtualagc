527236036187d4b5acbdd1d10e79e1a0b5e5f3d9
Ron Burkey <rburkey2005@earthlink.net>
2019-05-13 15:24:25 -0500

    Rethought the file structure used to represent "find tables" in csv format, reworked the existing csv files, and created a new one (for 2003993).  Created the drilldown.py program to actually perform the drilldowns.  It's only good enough right now to do the top level of a drilldown without descending recursively, but it seems to be able to produce a pretty fair HTML file from that, which could be inserted onto the website.  Of course, I want to make it work a lot better before doing that.

--- DrawingTree/README.txt
+++ DrawingTree/README.txt
@@ -21,21 +21,56 @@ AgcDrawingIndexXXXXXX.html, merged and sorted.  It has the fields
 
 	URL DRAWINGNUMBER REV DOCTYPE SHEETNUM FRAMENUM TITLE NOTES
 
-The sorting uses the keys from fields 2 (DRAWINGNUMBER) through
+Sorting uses the keys from fields 2 (DRAWINGNUMBER) through
 6 (SHEETNUM), in that order.
 
 There is a separate DRAWINGNUMBERPLUSREF.csv file for each drawing 
 relevant to this effort of expanding the drawing trees ... i.e., for each
-assembly.  It is essentially the same as the find-table in the associated 
-drawing, with the data ordered in the same way.  I.e., find numbers 
-increase from bottom to top, and dash numbers increase from right to left.  
-The column headers are the dash numbers.  The cell entries the same as the 
-cells in the drawing. In general those cell entries will be 
-DRAWINGNUMBER[-DASHNUMBER] (with no REV), though I allow also the syntax 
-of having several such items within the same cell, joined by " or ".
+assembly.  It is essentially the same as the "find" table in the associated 
+drawing, though with some additional flexibility in ordering.  The 
+first line of the file describes the type of data in each column, but 
+other than that restriction the columns and rows can be in any convenient
+order.  Usually, but not always, it will be most convenient for the 
+rows and columns to be in the same order as in the drawing itself, because
+that allows for the easiest data entry and proofing.
 
-I expect there will be software (presently TBD) that uses these files
-as input but produces HTML for the website.
+The column headings are the dash-number of the configuration described by
+the column, and will almost always be of the form "-011", "-021", "-031",
+and so forth.  (If these are shortened to "-11", "-21", ..., that's okay,
+since the post-processing software will pad the fields properly anyway.)
+Other possible column headings are:
+
+	"FIND"		The find-numbers of the rows.
+	"DRAWING"	The drawing numbers appearing in the rows.
+	"QTY"		The quantities of the assemblies in the rows.
+	"TITLE"		The titles of the drawings/assemblies in the rows.
+	"STRIKE"	If non-empty (say, "X") indicates a struck-out row.
+			(I.e., discard the row.)
+
+There are find-tables in the drawings of two essentially different types,
+and the post-processing software handles both types, which it distinguishes
+by the presence of the DRAWING/QTY columns:
+
+	DRAWING present, QTY absent:	The quantities are listed in the cells.
+	DRAWING absent, QTY present:	The drawing-numbers are listed in the cells.
+
+Wherever possible, the post-processing software takes drawing/assembly titles
+from the drawings.csv file.  However, it is still advisable to have the TITLE
+column, in case the corresponding drawing happens to be missing from the collection
+and thus to be missing from drawings.csv.
+
+In some cases, the DRAWING column (or the drawing numbers listed in the cells)
+may have several equally-valid alternatives, in which case that are separated
+by " or ".  For example, "DRAWING1 or DRAWING2 or DRAWING3".
+
+Sometimes, the part number is formed by some complicated rule which is difficult
+to specify properly in this kind of format, such as "Look up in table X of
+drawing Y, using table Z".  Therefore, the drawing number sometimes cannot 
+necessarily be generated automatically by software and is simply left as a 
+textual description of the rule.
+
+The post-processing software is drilldown.py, so look in its source code for
+additional documentation.
 
 Finally, in terms of how to produce the file drawings.csv, I used to have
 a fairly manually-intensive method here that involved cut-and-pasting from
---
+++ DrawingTree/drilldown.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python3
+# This program is supposed to be able to take an assembly number 
+# (from an assembly in the Apollo G&N system), and combine
+# its find-table file (SOMETHING.csv) with the obligatory 
+# drawings.csv file, producing a drawing drilldown from it.  
+# The output in TBD format.
+
+import sys
+import os
+
+if len(sys.argv) > 1:
+	assemblyName = sys.argv[1]
+	assembly = assemblyName.split("-")
+	if len(assembly) not in [1, 2]:
+		print("Assembly must be drawing[-configuration].", file=sys.stderr)
+		sys.exit(1)
+	drawing = assembly[0]
+	if len(assembly) == 2:
+		rawConfiguration = assembly[1]
+	else:
+		rawConfiguration = "000"
+	if not drawing.isdigit() or not rawConfiguration.isdigit():
+		print("Drawing number and/or configuration must be all digits.", file=sys.stderr)
+		sys.exit(1)
+	drawing = int(drawing)
+	configuration = -int(rawConfiguration)
+else:
+	print("No assembly specified on command line.", file=sys.stderr)
+	sys.exit(1)
+	
+
+# First step: read drawings.csv and keep it in a dictionary.
+drawings = { }
+f = open('drawings.csv', 'r')
+for line in f:
+	fields = line.strip().split("\t")
+	url = fields[0]
+	drawingNumber = fields[1]
+	revision = fields[2]
+	if len(revision) < 2:
+		revision = " " + revision
+	type = int(fields[3])
+	sheet = fields[4]
+	frame = fields[5]
+	title = fields[6]
+	if type != 1:
+		continue
+	if drawingNumber in drawings and revision <= drawings[drawingNumber]["revision"]:
+		continue
+	drawings[drawingNumber] = { "revision": revision.strip(), "url": url, "title": title }
+f.close()
+
+#for key in drawings:
+#	print(key + " " + str(drawings[key]))
+
+# Returns a dictionary with the contents of an assembly, or else an empty dictionary
+# if the find-table file wasn't found.
+def readFindTable(drawing, configuration):
+	findTable = {}
+	# First need to read the current folder to for all files with names of the form
+	# 	drawing + rev + ".csv"
+	# to determine the one with the highest rev.
+	highestRev = ""
+	files = os.listdir(".")
+	for file in files:
+		fields = file.split(".")
+		if len(fields) != 2 or fields[1] != "csv":
+			continue
+		basename = fields[0]
+		if len(basename) < 8:
+			continue
+		testDrawing = basename[:7]
+		if not testDrawing.isdigit():
+			continue
+		if int(testDrawing) != drawing:
+			continue
+		rev = basename[7:] 
+		if len(rev) not in [1, 2]:
+			continue
+		if rev[0] != "-" and not (rev[0] >= "A" and rev[0] <= "Z"):
+			continue
+		if len(rev) == 2 and rev[1] != "-" and not (rev[1] >= "A" and rev[1] <= "Z"):
+			continue
+		if len(rev) < len(highestRev):
+			continue
+		if rev < highestRev:
+			continue
+		highestRev = rev
+	if highestRev == "":
+		return findTable
+	filename = str(drawing) + highestRev + ".csv"
+	#print(filename, file=sys.stderr)
+	
+	# Now, read the file containing the find table.
+	f = open(filename, "r")
+	first = True
+	find = 1
+	for line in f:
+		fields = line.strip("\n").split("\t")
+		if first:
+			first = False
+			headings = fields
+			if "DRAWING" in headings and "QTY" not in headings:
+				qtyTable = True
+			elif "DRAWING" not in headings and "QTY" in headings:
+				qtyTable = False
+			else:
+				print("Cannot distinguish table type from DRAWING/QTY columns.", file=sys.stderr)
+				break
+			numConfigs = 0
+			for field in fields:
+				if field != ["FIND", "DRAWING", "QTY", "TITLE", "STRIKE"]:
+					numConfigs != 1
+			#print(headings)
+		else:
+			row = {}
+			found = False
+			titleField = -1
+			currentDrawing = []
+			if "FIND" not in headings:
+				currentFind = str(find) 
+				find += 1
+			for n in range(0,len(headings)):
+				if headings[n] == "FIND":
+					currentFind = fields[n]
+					if currentFind == "":
+						break
+				elif headings[n] in ["DRAWING", "QTY", "TITLE", "STRIKE"]:
+					if headings[n] == "DRAWING":
+						currentDrawing = fields[n].split(" or ")
+					row[headings[n]] = fields[n]
+				elif int(headings[n]) == configuration or (numConfigs == 1 and configuration in [0, -11]):
+					row[headings[n]] = fields[n]
+					if not qtyTable or fields[n] == "X" or fields[n] == "AR" or (fields[n].isdigit() and int(fields[n]) > 0):
+						if not (not qtyTable and fields[n] == ""):
+							found = True
+					if not qtyTable:
+						currentDrawing = fields[n].split(" or ")
+			row["DRAWING"] = currentDrawing
+			if found and ("STRIKE" not in headings or row["STRIKE"] == "") and currentFind != "":
+				row["URL"] = []
+				for d in currentDrawing:
+					fields = d.split("-")
+					if fields[0] in drawings:
+						row["TITLE"] = drawings[fields[0]]["title"]
+						row["URL"].append(drawings[fields[0]]["url"])
+					else:
+						row["URL"].append("")
+				if qtyTable or int(row["QTY"]) > 0:
+					findTable[currentFind] = row	
+	f.close()
+	
+	# Yay!  All done.
+	return findTable
+
+# Now read the assembly-specific drawing.
+findTable = readFindTable(drawing, configuration)
+
+# Write the html header.
+f = open("top.html", "r")
+lines = f.readlines()
+f.close()
+sys.stdout.writelines(lines)
+
+print("<script type=\"text/javascript\">")
+print("document.write(headerTemplate.replace(\"@TITLE@\",\"G&N Assembly Drill-down\").replace(\"@SUBTITLE@\",\"Assembly " + assemblyName + "\"))")
+print("</script>")
+
+print("<ul>")
+for n in range(0,200):
+	for c in ["", "A", "B", "C", "D"]:
+		key = str(n) + c
+		if key in findTable:
+			line = "<li>"
+			line += str(n) + ":  "
+			for n in range(0,len(findTable[key]['DRAWING'])):
+				if n > 0:
+					line += " or "
+				if findTable[key]["URL"][n] != "":
+					line += "<a href=\"" + findTable[key]["URL"][n] + "\">" + findTable[key]["DRAWING"][n] + "</a>"
+				else:
+					line += findTable[key]["DRAWING"][n]
+			line += " &mdash; " + findTable[key]["TITLE"]
+			line += "</li>"
+			print(line)
+print("</ul>")
+
+# Write the html footer.
+f = open("bottom.html", "r")
+lines = f.readlines()
+f.close()
+sys.stdout.writelines(lines)
+
