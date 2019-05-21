65d1a607d1a583324908e771c6d2b8f16566c12e
Ron Burkey <rburkey2005@earthlink.net>
2019-05-21 15:23:00 -0500

    The makeAllDrilldowns.sh now drills the Block II CM files in addition the previously-supported LM files by default.  It also allows instead specifying assemblies on the command line to override the default (all LM and CM).  Finally, it only overwrites the HTML files for the website if those files have actually changed.

--- DrawingTree/makeAllDrilldowns.sh
+++ DrawingTree/makeAllDrilldowns.sh
@@ -1,13 +1,57 @@
 #!/bin/bash
-# Use drilldown.py to create all G&N drilldown pages.
+# Use drilldown.py to create all G&N drilldown pages.  While it 
+# actually builds every G&N assembly, it doesn't overwrite the
+# web directory with any of their HTML unless that HTML has 
+# actually changed.  So it's safe to build as many times as you
+# like.
+#
+# If you put any command-line arguments, those will be treated
+# as names of assemblies, and it will drill down those assemblies
+# instead of the the default (all G&N assemblies).
 
-for a in `seq -w 21 10 181`
-do
-	if [[ "$a" != "101" && "$a" != "041" ]]
+dest=~/git/virtualagc-web
+function drilldown {
+	assembly=$1
+	echo Drilling $assembly ...
+	./drilldown.py $assembly >temp.html
+	if diff --brief temp.html $dest/$assembly.html
 	then
-		assembly=6014999-$a
-		echo $assembly
-		./drilldown.py $assembly >~/git/virtualagc-web/$assembly.html
+		echo No change in $assembly
+	else
+		cp -a temp.html $dest/$assembly.html
 	fi
-done
+}
 
+if [[ "$1" == "" ]]
+then
+
+	# LEM
+	for a in `seq -w 21 10 181`
+	do
+		if [[ $a != 101 && $a != 041 ]]
+		then
+			drilldown 6014999-$a
+		fi
+	done
+	
+	# Block II CM
+	for a in `seq -w 11 10 231`
+	do
+		if [[ $a != 041 ]]
+		then
+			drilldown 2014999-$a
+		fi
+	done
+	
+	# Block I CM
+	# ... TBD ...
+
+else
+
+	while [[ "$1" != "" ]]
+	do
+		drilldown $1
+		shift
+	done
+
+fi
