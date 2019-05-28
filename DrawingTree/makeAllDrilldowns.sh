13b608d1e1d38519076723a28d729a6d023377a8
Ron Burkey <rburkey2005@earthlink.net>
2019-05-28 09:22:45 -0500

    Changed makeAllDrilldowns.sh so that, by default, it rebuilds the master drawing index prior to doing drilldowns.  However, it only does so if there are changes to it.

--- DrawingTree/makeAllDrilldowns.sh
+++ DrawingTree/makeAllDrilldowns.sh
@@ -5,10 +5,23 @@
 # actually changed.  So it's safe to build as many times as you
 # like.
 #
+# Prior to performing the drilldowns, it rebuilds the master
+# drawing index.  However, it only changes files drawings.csv
+# and tipuesearch_content.js if their content has changed.
+#
 # If you put any command-line arguments, those will be treated
 # as names of assemblies, and it will drill down those assemblies
-# instead of the the default (all G&N assemblies).
+# instead of the the default (all G&N assemblies).  It will skip
+# rebuilding the master drawing index in this case, since the 
+# assumption is you're using this method to get stuff done fast.
 
+if [[ ! -f drilldownCompare.py ]]
+then
+	echo This must be run from the DrawingTree directory.
+	exit 1
+fi
+
+current=`pwd`
 dest=~/git/virtualagc-web
 function drilldown {
 	assembly=$1
@@ -17,15 +30,36 @@ function drilldown {
 	if diff --brief temp.html $dest/$assembly.html
 	then
 		echo No change in $assembly
+		rm temp.html
 	else
 		#meld temp.html $dest/$assembly.html
 		#exit
-		cp -a temp.html $dest/$assembly.html
+		mv temp.html $dest/$assembly.html
 	fi
 }
 
 if [[ "$1" == "" ]]
 then
+
+	# Rebuild master drawing index.
+	cd $dest
+	cat AgcDrawingIndex*.html | AgcDrawingIndex.py >temp.csv
+	if diff --brief temp.csv $current/drawings.csv
+	then
+		echo No change in drawings.csv
+		rm temp.csv
+	else
+		MakeTipueSearch.py <temp.csv >temp.js
+		if diff --brief temp.js TipueSearch/tipuesearch_content.js
+		then
+			echo No change in tipuesearch_content.js
+			rm temp.js
+		else
+		 	mv temp.js TipueSearch/tipuesearch_content.js
+		fi
+		move temp.csv $current/drawings.csv
+	fi
+	cd $current
 
 	# LEM
 	for a in `seq -w 21 10 181`
