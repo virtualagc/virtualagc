ab517483ec28cf4b3563b4d8fb1598df6674d203
Ron Burkey <rburkey2005@earthlink.net>
2019-06-02 10:51:53 -0500

    Added some useful links between Block I and yaDSKY pages to associated engineering drawings.  Added a few mil-specs in the process.

--- TipueSearch/tipuesearch_content.js
+++ TipueSearch/tipuesearch_content.js
@@ -15449,6 +15449,8 @@ var tipuesearch = {"pages": [
 	{"title":"JDC18083A-1-1-1" ,"text":"CLEANING OF CONNECTORS AND COMPONENTS" ,"url":"https://archive.org/stream/AgcApertureCardsBatch10Images#page/n270/mode/1up"},
 	{"title":"MIL-A-5092--1-1-1" ,"text":"ADHESIVE, RUBBER BASE, GENERAL PURPOSE" ,"url":"Documents/assist.dla.mil/MMM_A_1617B.pdf"},
 	{"title":"MIL-A-8625F-1-1-1" ,"text":"ANODIC COATINGS FOR ALUMINUM AND ALUMINUM ALLOWS" ,"url":"Documents/assist.dla.mil/MIL_A_8625F.pdf"},
+	{"title":"MIL-C-25050A-1-1-1" ,"text":"COLORS, AERONAUTICAL LIGHTS AND LIGHTING EQUIPMENT, GENERAL SPECIFICATIONS FOR" ,"url":"Documents/assist.dla.mil/MIL_C_25050A.pdf"},
+	{"title":"MIL-D-70327--1-1-1" ,"text":"DRAWINGS, ENGINEERING AND ASSOCIATED LISTS" ,"url":"Documents/assist.dla.mil/MIL-D-70327.pdf"},
 	{"title":"MIL-I-15126F-1-1-1" ,"text":"INSULATION TAPE, ELECTRICAL, PRESSURE SENSITIVE ADHESIVE AND PRESSURE SENSITIVE THERMOSETTING ADHESIVE" ,"url":"Documents/assist.dla.mil/MIL_I_15126F.pdf"},
 	{"title":"MIL-I-23594C-1-1-1" ,"text":"INSULATION TAPE, ELECTRICAL, HIGH TEMPERATURE, POLYTETRAFLOUROETHYLENE, PRESSURE-SENSITIVE" ,"url":"Documents/assist.dla.mil/MIL_I_23594C.pdf"},
 	{"title":"MIL-I-631D-1-1-1" ,"text":"INSULATION, ELECTRICAL, SYNTHETIC - RESIN COMPOSITION, NONRIGID" ,"url":"Documents/assist.dla.mil/MIL_I_631D.pdf"},
