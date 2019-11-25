a290db51ea8b0c8163945f4ee604c128585d2b81
Ron Burkey <rburkey2005@earthlink.net>
2019-11-25 16:50:52 -0600

    Added unpunchAllGAEC.sh, along with some more work on unpunchGAEC.py, but it still doesn't seem to be working in terms of correctly parsing the scan filenames in all cases.

--- Scripts/unpunchGAEC.py
+++ Scripts/unpunchGAEC.py
@@ -18,18 +18,18 @@
 # not sure what the actual distinction is.  In terms of that cards, what
 # you have is this:
 #
-#	If primary:
+#0	If primary:
 #		1 col		Space
 #		1 col		Drawing size (can be a space)
 #	If secondary:
 #		2 cols		"EO"
-#	15 cols		Drawing number (right-filled with space)
-#	5 cols		Code identification number (5 digits)
-#	3 cols		Sheet number (usually spaces)
-#	2 cols		Revision (letter, left-padded with space; usually spaces for secondary)
-#	3 cols		Number of sheets (3 digits; usually spaces for secondary)
-#	2 cols		Frame number (2 digits)
-#	2 cols		Number of frames (2 digits)
+#2	15 cols		Drawing number (right-filled with space)
+#17	5 cols		Code identification number (5 digits)
+#22	3 cols		Sheet number (usually spaces)
+#25	2 cols		Revision (letter, left-padded with space; usually spaces for secondary)
+#27	3 cols		Number of sheets (3 digits; usually spaces for secondary)
+#30	2 cols		Frame number (2 digits)
+#32	2 cols		Number of frames (2 digits)
 #	9 cols		Subdivided as follows:
 #		1 col	Camera number (1 digit; usually spaces for secondary)
 #		5 cols	Roll number (digits, terminated by ".", right-filled with space; usually spaces for secondary)
@@ -101,6 +101,7 @@ if len(sys.argv) > 2:
 dupList = {}
 htmlList = {}
 for line in sys.stdin:
+	prefix = line[:2]
 	line = line.strip()
 	if line[-8:] == "-000.png":
 		line = line[:-8]
@@ -109,84 +110,40 @@ for line in sys.stdin:
 	copy = ""
 	if line[-7] == "(" and line[-5:] == ").PDF":
 		copy = " (copy " + line[-6:-4]
-	docType = line[:2].strip()
-	if docType == "":
-		docType = "00"
-	frameNumber = line[2:4].strip()
+	
+	if prefix == "EO":
+		docType = "02"
+	elif prefix == "  ":
+		docType = "01"
+		line = "  " + line 
+	else:
+		docType = "01"
+		line = " " + line
+	frameNumber = line[30:32].strip()
 	if frameNumber == "":
 		frameNumber = "1"
-	numFrames = line[4:6].strip()
+	numFrames = line[32:34].strip()
 	if numFrames == "":
 		numFrames = "1"
-	if mh:
-		docNumber = "MH01-" + line[6:15]
+	docNumber = line[2:17].strip().replace("-","_")
+	sheetNum = line[22:25].strip()
+	if sheetNum == "":
 		sheetNum = "001"
-	elif line[6:10] == "JDC ":
-		docNumber = "JDC" + line[10:15]
-		sheetNum = "001"
-	else:
-		docNumber = line[6:13]
-		sheetNum = line[14:17]
-	if sheetNum[1] != " " and sheetNum[2] == " ":
-		sheetNum = sheetNum[:2] + "8"
-	if sheetNum[1] == " ":
-		sheetNum = sheetNum[0] + "0" + sheetNum[2]
-	if sheetNum[0] == " ":
-		sheetNum = "0" + sheetNum[1:]
-	if sheetNum == "00 ":
-		sheetNum = "001"
-	if line[19].isalpha():
-		revision = line[19:21].strip()
-	else:
-		revision = line[20].strip()
+	while len(sheetNum) < 3:
+		sheetNum = "0" + sheetNum
+	revision = line[25:27].strip()
 	if revision == "":
 		revision = "-"
 	if len(revision) == 1:
 		revision = "-" + revision
-	tdrr = line[21:26]
-	numSheets = line[27:29].strip()
-	if numSheets == "":
-		numSheets = "1"
-	title = re.sub(r" +", " ", line[30:52].strip())
-	group = line[54:57]
-	while len(sheetNum) < 3:
-		sheetNum = "0" + sheetNum
-	while len(frameNumber) < 2:
-		frameNumber = "0" + frameNumber
-
-	if correctMetadata:
-		# Whether it's a problem in the system that initially punched
-		# the aperture cards or whether it's a problem with the scanner
-		# software I don't know, but the fact is that the metadata the
-		# scanner reads from the cards departs from the (correct) printing
-		# on the card, with semi-systematic replacements of some characters
-		# in some fields by other characters.  For example, almost all
-		# (but not QUITE all!) revision fields "Y" are replaced by "0",
-		# and all "H" characters in the title field are replaced by "&".
-		# But there are real "&" characters in the title field as well,
-		# sometimes, though many less of them.  Yikes!  The following 
-		# block of code tries to fix the most egregious errors, but it's
-		# simply impossible to fix everything in an automated way.
-		# So if you have (for example) at title "NAV&MAIN DSKY", it's 
-		# going to end up being "fixed" by this code as "NAVHMAIN DSKY",
-		# and there's just nothing I can do about it.  Fortunately, as
-		# far as I know right now, all of the unfixable errors are in the
-		# titles, and those will eventually be manually entered from data
-		# on the drawings anyway.  I.e., the titles have been taken initially
-		# from card data for expedience, but will eventually be corrected.
-		docNumber = docNumber.replace(" ", "8")
-		revision = revision.replace("&","H").replace("0", "Y")
-		tdrr = tdrr.replace(" ", "8")
-		while revision[:1] == " ":
-			revision = revision[1:]
-		while len(sheetNum) > 3 and sheetNum[:1] in [ " ", "0" ]:
-			sheetNum = sheetNum[1:]
-		while len(frameNumber) > 2 and frameNumber[:1] in [ " ", "0" ]:
-			frameNumber = frameNumber[1:]
-		title = title.replace("&", "H").replace("0", "Y").replace("-", "Q").replace(" H ", " & ")
-
 	revision_ = revision.replace("-", "_")
-	basename_ = docNumber + revision_ + "_" + docType + "_" + sheetNum + "_" + frameNumber + "_" + title + copy
+	numSheets = line[27:30].strip()
+	if numSheets == "":
+		numSheets = "01"
+	while len(numSheets) < 2:
+		numSheets += "0" + numSheets
+
+	basename_ = docNumber + revision_ + "_" + docType + "_" + sheetNum + "_" + frameNumber + "_" + line.replace(".PDF", "") + copy
 	basename = basename_
 	dup = 1
 	while basename in htmlList:
@@ -199,13 +156,13 @@ for line in sys.stdin:
 		"docType" : docType,
 		"sheetNum" : sheetNum,
 		"frameNumber" : frameNumber,
-		"title" : title,
+		"title" : "",
 		"copy" : copy, 
 		"dup" : dup,
 		"numFrames" : numFrames,
 		"numSheets" : numSheets,
-		"tdrr" : tdrr, 
-		"group" : group,
+		"tdrr" : "", 
+		"group" : "",
 		"line" : rawLine
 	}
 	if dup > 1:
@@ -223,7 +180,7 @@ for n in dupList:
 		print("xreader '" + m["line"] + "' &")
 print("'")
 
-sys.stderr.write("<table class=\"drawingIndex\"><tbody>\n<tr>")
+sys.stderr.write("<table class=\"gaecIndex\"><tbody>\n<tr>")
 sys.stderr.write("<td><b>Link</b></td>")
 sys.stderr.write("<td><b>Drawing</b></td>")
 sys.stderr.write("<td><b>Rev</b></td>")
