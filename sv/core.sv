1503bd4fdfa19de9869ef4d5cdf0dc1e1d7fc1a2
Christopher Bernard <cabernar@andrew.cmu.edu>
2022-03-16 18:10:26 -0400

    made changes to tope and core so they connect beter

--- sv/core.sv
+++ sv/core.sv
@@ -3,9 +3,9 @@
 `include "internal_defines.vh"
 module Core
   (input logic clock, reset_n,
-   input logic [14:0] ROM_read_data, RAM_read_data, IO_read_data,
+   input logic [14:0] ROM_pc_data, ROM_constant_data, RAM_read_data, IO_read_data,
    output logic [14:0] RAM_write_data, IO_write_data,
-   output logic [14:0] ROM_address, RAM_read_address, RAM_write_address,
+   output logic [14:0] ROM_pc_address, ROM_constant_address, RAM_read_address, RAM_write_address,
    output logic [2:0] IO_read_sel, IO_write_sel,
    output logic RAM_write_en, stall, halt);
 
