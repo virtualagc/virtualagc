63b01dc91b3f9d67d15b03634be9cd5cd51dcdfe
Christopher Bernard <cabernar@andrew.cmu.edu>
2022-03-22 18:56:51 -0400

    found syntax errors in top.sv

--- sv/top.sv
+++ sv/top.sv
@@ -1,18 +1,17 @@
+
 module TB;
-  logic clock, reset_n;
+  logic clock, reset_n, finish;
 
   initial begin
     clock = 1'b0;
     reset_n = 1'b1;
-    reset_n <= 1'b0;
-    resetN <= #1 1'b1;
+    reset_n <= #1 1'b1;
     forever #5 clock = ~clock;
   end
 
   //TODO instantiate ROM, and IO
   //data is input data
 
-  logic clock, reset_n,
   logic [14:0] ROM_pc_data, ROM_constant_data, RAM_read_data, IO_read_data;
   logic [14:0] RAM_write_data, IO_write_data;
   logic [14:0] ROM_pc_address, ROM_constant_address, RAM_read_address, RAM_write_address;
@@ -36,8 +35,6 @@ module TB;
 
   initial begin
     #100000
-    $display("time = %d, ans = %h, state %s" , dut.count, dut.finalSum, dut.s1.f1.state);
-    $display("@%0t: Error timeout!", $time);
     $finish;
   end
 endmodule : TB
