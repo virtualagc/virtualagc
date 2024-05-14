b842da55020c767de0d7815df2209ba3507caf4d
Ron Burkey <rburkey2005@earthlink.net>
2024-05-14 07:25:06 -0500

    Added source code for Comanche 67.

--- DocumentLibraryDatabase/buildLibraryPage.py
+++ DocumentLibraryDatabase/buildLibraryPage.py
@@ -583,7 +583,7 @@ else:
     <tr align="center"><td>Apollo 9</td><td>yes</td><td>fine</td><td>no</td><td>no</td></tr>
     <tr align="center"><td>Apollo 10</td><td>yes</td><td>yes</td><td>no</td><td>no</td></tr>
     <tr align="center"><td>Apollo 11</td><td>yes</td><td>yes</td><td>yes</td><td>no</td></tr>
-    <tr align="center"><td>Apollo 12</td><td>binary</td><td>yes</td><td>yes</td><td>no</td></tr>
+    <tr align="center"><td>Apollo 12</td><td>yes</td><td>yes</td><td>yes</td><td>no</td></tr>
     <tr align="center"><td>Apollo 13</td><td>no</td><td>yes</td><td>no</td><td>no</td></tr>
     <tr align="center"><td>Apollo 14</td><td>no</td><td>yes</td><td>no</td><td>no</td></tr>
     <tr align="center"><td>Apollo 15</td><td>yes</td><td>yes</td><td>yes</td><td>no</td></tr>
