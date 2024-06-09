1df9f64d5c9246f37fe940c25483ced0a3599f09
Ron Burkey <rburkey2005@earthlink.net>
2024-06-09 09:12:40 -0500

    More of the same.

--- XCOM-I/sim360-source/simulate/system.c
+++ XCOM-I/sim360-source/simulate/system.c
@@ -15,12 +15,27 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
-#include <sys/time.h>
 #include <sys/resource.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <ctype.h>
+
+#ifndef _WIN32
+// Windows VS doesn't have sys/time.h.
+#include <sys/time.h> // For gettimeofday().
+#else
+struct timeval {
+   time_t      tv_sec;     /* seconds */
+   suseconds_t tv_usec;    /* microseconds */
+};
+int gettimeofday(struct timeval *tv, void *tz) {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  tv->tv_sec = ts.tv_sec;
+  tv->tv_usec = ts.tv_nsec / 1000;
+}
+#endif
 
 #include "sim.h"
 
