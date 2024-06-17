be1473db1b3e49735bb3832c5b017fcae17048d6
Ron Burkey <rburkey2005@earthlink.net>
2024-06-17 08:52:11 -0500

    OPT pass of HAL/S-FC may be working now.

---
+++ yaShuttle/Source Code/PASS.REL32V0/OPT.PROCS/patch5.c
@@ -0,0 +1,20 @@
+/*
+ * A C-language patch for CALL INLINE #5-8 in INT_TO_SCALAR procedure of
+ * OPT pass of HAL/S-FC.
+ *
+ * My interpretation of the IBM 360 machine code:
+ *
+ * CALL INLINE("2B",0,0);         SDR 0,0     Load FR0 with 0.0
+ * CALL INLINE("58",1,0,FOR_DW);  L 1,FOR_DW  Load address of DW[0] into GR1.
+ * CALL INLINE("6A",0,0,1,0);     AD 0,0(1)   Add (normalized) DW[0],DW[1] to FR0.
+ * CALL INLINE("60",0,0,1,0);     STD 0,0(1)  Store FR0 into DW[0],DW[1]
+ *
+ * I.e., take a 2's-complement number from DW[1], convert it to an IBM hex
+ * float, and put the result back into DW[0],DW[1].
+ */
+
+uint32_t msw, lsw, addressDW = getFIXED(mFOR_DW);
+double N = getFIXED(addressDW + 4);
+toFloatIBM(&msw, &lsw, N);
+putFIXED(addressDW + 0, msw);
+putFIXED(addressDW + 4, lsw);
