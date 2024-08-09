fc25482e4528e7fd29b224b038007885b8ea6516
Ron Burkey <rburkey2005@earthlink.net>
2024-08-08 19:06:04 -0500

    Again forgot to include the runtime library in the commit of changes to the runtime library.

--- HALSFC/PASS1/runtimeC.c
+++ HALSFC/PASS1/runtimeC.c
@@ -34,12 +34,41 @@
 #include <math.h>
 #include <ctype.h>
 #ifdef _WIN32
+	// All the stuff here was just cut-and-pasted from googling "windows
+	// replacement for (whatever)".
 	#include <stdarg.h>
-	typedef struct timeval {
-	  long tv_sec;
-	  long tv_usec;
-	} timeval;
 	#define F_OK 0
+	#define strcasecmp _stricmp
+	#define WIN32_LEAN_AND_MEAN
+	#include <Windows.h>
+	#include <stdint.h> // portable: uint64_t   MSVC: __int64
+
+	// MSVC defines this in winsock2.h!?
+	typedef struct timeval {
+		long tv_sec;
+		long tv_usec;
+	} timeval;
+
+	int gettimeofday(struct timeval * tp, struct timezone * tzp)
+	{
+		// Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+		// This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+		// until 00:00:00 January 1, 1970
+		static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+		SYSTEMTIME  system_time;
+		FILETIME    file_time;
+		uint64_t    time;
+
+		GetSystemTime( &system_time );
+		SystemTimeToFileTime( &system_time, &file_time );
+		time =  ((uint64_t)file_time.dwLowDateTime )      ;
+		time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+		tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+		tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+		return 0;
+	}
 #else
 	#include <unistd.h>
 #endif
