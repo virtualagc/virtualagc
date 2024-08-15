970ed49c1109637813b60a7c08873278aa1be86e
Ron Burkey <rburkey2005@earthlink.net>
2024-08-15 12:37:05 -0500

    Modified `fixedToDescriptor` in runtimeC.c, trying to work around clang problem on Msys2.

--- XCOM-I/runtimeC.c
+++ XCOM-I/runtimeC.c
@@ -1488,7 +1488,7 @@ putCHARACTER(uint32_t address, descriptor_t *str)
 
 descriptor_t *
 fixedToCharacter(int32_t i) {
-  return cToDescriptor(NULL, "%d", i);
+  return cToDescriptor(NULL, "%d", (int) i);
 }
 
 // Convert a BIT(1) through BIT(32) to FIXED.  As near as I can figure (see
