41046ab1eed868c4463cd9699a311b5417315490
Ron Burkey <rburkey2005@earthlink.net>
2024-11-14 06:42:27 -0600

    Update for the ASM101S regression tester to allow keeping the full assembly listings.

--- ASM101S/regressionASM101S.sh
+++ ASM101S/regressionASM101S.sh
@@ -4,16 +4,23 @@
 # contemporary assembly listings are available, and doing a byte-by-byte
 # comparison of the generated code.
 
+
+
 cd "../yaShuttle/Source Code/PASS.REL32V0/RUNASM"
 for f in *.asm
 do 
     n=${f%.*}
+    echo -n -e "\033[0;33m$n\033[0m "
+    ASM101S.py --library=../RUNMAC --tolerable=4 --compare=../RUNLST/$n.txt $n.asm &>temp.lst
     if [[ "$1" != "" ]]
     then
-        echo -n -e "\033[0;33m$n\033[0m "
+        cp temp.lst $n.lst
     fi
-    ASM101S.py --library=../RUNMAC --tolerable=4 --compare=../RUNLST/$n.txt $n.asm \
-    | tail -n1 | grep --invert-match "0 bytes mismatched and 0 bytes missing"
+    tail -n1 temp.lst | grep --invert-match "0 bytes mismatched and 0 bytes missing"
 done
 echo ""
+if [[ "$1" != "" ]]
+then
+    echo 'Assembly-listing files *.lst were created.'
+fi
 cd - &>/dev/null
