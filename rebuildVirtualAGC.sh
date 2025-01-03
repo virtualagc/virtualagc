339b07f1ebb03def47260ee015a078147e4ac682
Ron Burkey <rburkey2005@earthlink.net>
2025-01-03 14:59:17 -0600

    Added more to VM rebuild script

--- rebuildVirtualAGC.sh
+++ rebuildVirtualAGC.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # This script is used on the VM VirtualAGC-VM64 to rebuild the entire
-# Virtual AGC sourcd tree, including items not normally automatically built.
+# Virtual AGC source tree, including items not normally automatically built.
 
+SOURCE_DIR="~/git/virtualagc"
+
+# Described in download.html#Linux
 # First, make all of the "normal" stuff that's usually automatic.
+cd "$SOURCE_DIR"
 make clean install clean
 
-# Now build some extra, non-standard stuff that is usually built manually.
-cd XCOM-I
+# Described in XPL-I.html.
+cd "$SOURCE_DIR"/XCOM-I
 rm sim360 &>/dev/null
 make sim360
 make
-cd ../yaShuttle/"Source Code"/PASS.REL32V0
+cd "$SOURCE_DIR"/yaShuttle/"Source Code"/PASS.REL32V0
 make -s XEXTRA=--quiet clean all regression
+
+# Described in ASM101S.html
+cd "$SOURCE_DIR"/ASM101S
+regressionASM101S.sh -v
+
+# Described in hal-s-compiler.html
+cd "$SOURCE_DIR"/yaShuttle/yaHAL-S
+make distclean all
 
