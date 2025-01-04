1b4536c43cf50481f731f8e3c7d9c1b28068407f
Ron Burkey <rburkey2005@earthlink.net>
2025-01-03 18:29:26 -0600

    Tweaks to VM update script

--- updateVirtualAGC-VM64.sh
+++ updateVirtualAGC-VM64.sh
@@ -3,6 +3,8 @@
 # by getting the latest version of the source tree and building it.  However,
 # it can potentially perform other tasks as well.
 
+LOG_FILE=/home/virtualagc/Desktop/Update.log
+
 echo The process of updating Virtual AGC should be harmless and easy,
 echo although it will probably take several minutes. But if you have  
 echo made changes yourself to the source-code tree, the update process 
@@ -13,15 +15,15 @@ read -p "Are you sure you're ready to proceed at this time (y/N)? " -n 1 -r
 if [[ "$REPLY" =~ ^[Yy]$ ]]
 then
         echo ""
-        cd ~/git/virtualagc
-        git stash
-        git stash drop
-        git pull
+        cd ~/git/virtualagc &> $LOG_FILE
+        git stash 2>&1 >> $LOG_FILE
+        git stash drop 2>&1 >> $LOG_FILE
+        git pull 2>&1 >> $LOG_FILE
         
         # The build stuff is separated out into an independent script, so
         # as to make sure that changes to the build procedure are captured
         # without a 2nd git pull.
-        time ./rebuildVirtualAGC.sh
+        time ./rebuildVirtualAGC.sh 2>&1 | tee -a $LOG_FILE
         
         echo "Terminated.  Hit ENTER key or close this window."
         read
