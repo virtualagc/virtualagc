58aa2cece9431c28ab7fd6c8303152b54c7d4cc8
Ron Burkey <rburkey2005@earthlink.net>
2025-01-05 07:21:07 -0600

    Added the vmDesktopIcons.sh script for creating electrical-schematic icons in VirtualAGC-VM64.

---
+++ Schematics/vmDesktopIcons.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script generates desktop icons for the VirtualAGC-VM64 virtual machine.
+# There's one icon per drawing number in Schematics/.  Each icon loads the 
+# associated drawing's top-level page into the KiCad schematic editor.
+
+home=/home/virtualagc
+desktop=$home/Desktop
+schematics=$home/git/virtualagc-schematics/Schematics
+cd $schematics
+if [[ ! -d $desktop ]]
+then
+        mkdir $desktop
+fi
+for f in [0-9]*
+do
+        if [[ -d "$f" ]]
+        then
+                echo '[Desktop Entry'] >temp.desktop
+                echo "Name=$f Schematics Viewer" >> temp.desktop
+                echo "GenericName=" >> temp.desktop
+                echo "Comment=" >> temp.desktop
+                echo "Exec=eeschema $schematics/$f/module.sch" >> temp.desktop
+                echo "Type=Application" >> temp.desktop
+                echo "Icon=eeschema" >> temp.desktop
+                echo "Terminal=false" >> temp.desktop
+                mv temp.desktop $desktop/$f.desktop
+        fi
+done
