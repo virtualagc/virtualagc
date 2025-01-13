e3de107b581d63463e4f879ae79df9a9f09447d3
Ron Burkey <rburkey2005@earthlink.net>
2025-01-13 17:30:06 -0600

    This is a dummy commit, for test purposes.

--- updateVirtualAGC-VM64.sh
+++ updateVirtualAGC-VM64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is used on the VM VirtualAGC-VM64 to update the local Virtual AGC
 # by getting the latest version of the source tree and building it.  However,
-# it can potentially perform other tasks as well.
+# it can potentially perform other tasks as well. 
 
 LOG_FILE=/home/virtualagc/Desktop/Update.log
 
