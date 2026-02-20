632d0f9fb66bb164ac85b340bb6e9af31dfdab8d
Ron Burkey <rburkey2005@earthlink.net>
2026-02-19 18:15:37 -0600

    Clarified some program comments in unLitfile.py.

--- yaShuttle/Source Code/PASS.REL32V0/unLitfile.py
+++ yaShuttle/Source Code/PASS.REL32V0/unLitfile.py
@@ -14,22 +14,28 @@ Two files are necessary for the decoding process: the litfile itself and an
 image of the memory space of the program that produced the litfile.  The latter
 is needed because the litfile only provides pointers into memory for the 
 textual data of string literals.  Numeric literals are provided directly in
-the litfile.
+the litfile.  When compiling a HAL/S file, after each compiler pass, and 
+specifically those passes capable of modifying the litfile, HALSFC
+produces the files you need for LITFILE.bin and MEMORY.gz:
+
+    After PASS1: litfile0.bin COMMON0.out.bin.gz
+    After OPT:   litfile2.bin COMMON2.out.bin.gz
+    After PASS2: litfile4.bin COMMON4.out.bin.gz
 
 The litfile data is packed into records of 1560 bytes each.  Each of those 
 records is formatted identically, as 3 pages of 130 4-byte records each.
-Each record contains data for 130 literals.  
+Each 1560-byte record contains data for 130 literals.  
 
-The 4-byte records on the first page are "types" of the literals:  big-endian 
-integers with the values 
+The 4-byte records on the first 520-byte page are "types" of the literals: 
+big-endian integers with the values 
     0 (for character-string literals)
     1 (for numeric literals)
     2 (for BIT(1) through BIT(32) literals).  
 (I believe there may be other types as well, such as 5, but I've never seen 
 them and don't know what they represent.)
 
-As for the other 2 pages, the data format in their 4-byte fields depends on
-the type of literal:
+As for the other 2 520-byte pages, the data format in their 4-byte fields 
+depends on the type of literal:
     Type 0: The 2nd page contains "string descriptors".  I.e., the first byte
             is the string length minus 1, while the other three bytes are 
             a 24-bit pointer into the memory image.  If all 4 bytes are 0, it
