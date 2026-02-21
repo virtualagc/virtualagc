95077220a0b95d7fc8a1949fc945dc270db88dc4
Ron Burkey <rburkey2005@earthlink.net>
2026-02-20 19:37:38 -0600

    Double-colon comments in batch file stopped working for some reason.

--- yaShuttle/Source Code/PASS.REL32V0/HALSFCp.bat
+++ yaShuttle/Source Code/PASS.REL32V0/HALSFCp.bat
@@ -1,8 +1,8 @@
-:: This batch script simply runs the Python script HALSFCp, giving it
-:: all of the command-line parameters unchanged, but changing the path to
-:: HALSFCp itself by replacing all \ by /.  This script is needed because it's
-:: so phenominally difficult in Windows to directly run a Python script from 
-:: the command line that isn't in the current working directory.
+REM This batch script simply runs the Python script HALSFCp, giving it
+REM all of the command-line parameters unchanged, but changing the path to
+REM HALSFCp itself by replacing all \ by /.  This script is needed because it's
+REM so phenominally difficult in Windows to directly run a Python script from 
+REM the command line that isn't in the current working directory.
 
 @echo off >nul
 for /f "tokens=*" %%i in ('where HALSFCp') do set HALSFCp=%%i
