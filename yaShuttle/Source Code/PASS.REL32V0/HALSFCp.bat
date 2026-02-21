082fb81a5a0ba5beb2ceefed4f0d79da39f7d27a
Ron Burkey <rburkey2005@earthlink.net>
2026-02-20 20:06:58 -0600

    Corrected a typo in a comment.

--- yaShuttle/Source Code/PASS.REL32V0/HALSFCp.bat
+++ yaShuttle/Source Code/PASS.REL32V0/HALSFCp.bat
@@ -2,7 +2,7 @@
 REM This batch script simply runs the Python script HALSFCp, giving it
 REM all of the command-line parameters unchanged, but changing the path to
 REM HALSFCp itself by replacing all \ by /.  This script is needed because it's
-REM so phenominally difficult in Windows to directly run a Python script from 
+REM so phenomenally difficult in Windows to directly run a Python script from 
 REM the command line that isn't in the current working directory.
 
 for /f "tokens=*" %%i in ('where HALSFCp.') do set HALSFCp=%%i
