456baeedda47c74de0d0257e1c9b549486a1e42d
Ron Burkey <rburkey2005@earthlink.net>
2026-05-14 11:51:15 -0500

    Fix for issue #1309.

--- yaShuttle/ported/PASS1.PROCS/PREPLITE.py
+++ yaShuttle/ported/PASS1.PROCS/PREPLITE.py
@@ -10,6 +10,14 @@ History:    2023-09-22 RSB  Ported from XPL
             2026-03-18 RSB  Now truncates floating-point to integer for `VALUE`
                             rather than rounding.
             2026-04-15 RSB  Wasn't detecting fractional parts correctly.
+            2026-05-14 RSB  Eliminated heuristic determination of 
+                            integer vs non-integer by introducing ibmFloat
+                            module for native HFP calculations, and matching the
+                            logic of PREPLITE.xpl if not the detail.  The ibmHex
+                            module is eliminated completely.  But note that this
+                            is *not* an implementation of issue #1306, because
+                            the virtual registers FR[n] remain native-Python
+                            floats rather than IBM HFP.
 '''
 
 import math
@@ -17,6 +25,7 @@ from xplBuiltins import *
 import g
 import HALINCL.COMMON as h
 from HALINCL.SAVELITE import SAVE_LITERAL
+from ibmFloat import ibm_dp_to_double, ibm_dp_from_double, ibm_dp_addsub
 
 '''
  /***************************************************************************/
@@ -69,7 +78,7 @@ def PREP_LITERAL():
     
         if g.EXP_OVERFLOW or goto == "NOT_EXACT":
             if goto == None:
-                g.FR[6] = fromFloatIBM(0x487FFFFF, 0xFFFFFFFF);
+                g.FR[6] = ibm_dp_to_double(0x487FFFFF, 0xFFFFFFFF);
             if goto == "NOT_EXACT":
                 goto = None
             g.VALUE = -1
@@ -80,7 +89,7 @@ def PREP_LITERAL():
         # TEMP1 = ADDR(NOT_EXACT)
         g.FR[6] = g.FR[0]  # p59_8 FR[0] was loaded by MONITOR(10)
         g.FR[0] = abs(g.FR[0]) # p59_10
-        scratch = g.FR[0] - fromFloatIBM(0x487FFFFF, 0xFFFFFFFF) # p59_12
+        scratch = g.FR[0] - ibm_dp_to_double(0x487FFFFF, 0xFFFFFFFF) # p59_12
         if scratch > 0: # p59_16
             goto = "NOT_EXACT"
             continue
@@ -94,18 +103,29 @@ def PREP_LITERAL():
             if g.FR[2] != 0: # p59_42
                 goto = "NOT_EXACT"
                 continue
-        else:
+        elif False:
             g.DW[6] = 0 
             g.DW[7] = int(math.trunc(g.FR[0]))
             if abs(g.FR[0]-g.DW[7]) > 1E-10:
                 goto = "NOT_EXACT"
                 continue
+        else:
+            msw, lsw = ibm_dp_from_double(g.FR[0])
+            value = (msw << 32) | lsw
+            awresult = ibm_dp_addsub(value, 0x4E00000000000000, 0, 0)
+            adrresult = ibm_dp_addsub(awresult, 0, 0, 1)
+            sdrresult = ibm_dp_addsub(value, adrresult, 1, 1)
+            if (sdrresult & 0x00FFFFFFFFFFFFFF) != 0:
+                goto = "NOT_EXACT"
+                continue
+            g.DW[6] = 0 
+            g.DW[7] = awresult & 0xFFFFFFFF
         g.VALUE = g.DW[7] # p59_44, 48
     
     #SAVE_NUMBER:    
     g.traceInline("PREP_LITERAL p76")
-    g.DW[6], g.DW[7] = toFloatIBM(g.FR[6]) # p76_0, 4
+    g.DW[6], g.DW[7] = ibm_dp_from_double(g.FR[6]) # p76_0, 4
 
     #g.SYT_INDEX = SAVE_LITERAL(1, h.TABLE_ADDR);
-    g.SYT_INDEX = SAVE_LITERAL(1, g.fromFloatDW01());
+    g.SYT_INDEX = SAVE_LITERAL(1, ibm_dp_to_double(g.DW[0], g.DW[1]))
     return
